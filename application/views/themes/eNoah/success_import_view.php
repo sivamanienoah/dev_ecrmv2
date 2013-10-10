08e3aab21257e1d66327952e7da1f72bf69d1e15
ramakrishnan <ramakrishnan@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2013-10-10 05:28:02 +0000

    import customer xls, csv & xlsx - format upload

--- application/views/themes/eNoah/success_import_view.php
+++ application/views/themes/eNoah/success_import_view.php
@@ -1,5 +1,5 @@
 <?php require ('tpl/header.php'); ?>
-
+<?php //echo "<pre>"; $arr = implode(",", $dupsemail);print_r( $arr);exit; ?>
 <div id="content">
     <!--<div id="left-menu">
 		<a href="customers/">Back To List</a>
@@ -7,20 +7,23 @@
     </div>-->
     <div class="inner">
     	<?php echo "$succcount Customer(s) Succesfully Uploaded"; ?>
-	<?php if(!empty($dupsemail)){?>
-	<div><b>The Following Customers Not updated in Customer List, because </b></div>
-	 <?php echo " The Customer Email is Already Exists in Customer List (s) <br /> $dupsemail ";
+	<?php if(!empty($dupsemail)){
+	$dupsemail_str = implode('<br/>', $dupsemail); ?>
+	<div><b>The Following Customers Not updated in Customer List, because below email ids are exits.</b></div>
+	 <?php echo " The Customer Email is Already Exists in Customer List (s) <br /> $dupsemail_str ";
 	}
 	?> 
 	
-    <?php if(!empty($required)){?>
+    <?php if(!empty($empty_error)){
+    $empty_error_str = implode('<br/>', $empty_error); ?>
 	<div><b>The Following Customers Not updated in Customer List, because </b></div>
-	 <?php echo " The Customer First Name or Last Name or Company or Region or Direct Phone or Email is missing <br /> $required ";  
+	 <?php echo " The Customer First Name or Last Name or Company or Region or Direct Phone or Email is missing <br /> $empty_error_str";  
 	}
 	?> 
-	<?php if(!empty($invalidemail)){?>
+	<?php if(!empty($invalidemail)){
+	 $invalidemail_str = implode('<br/>', $invalidemail); ?>
 	<div><b>The Following Customers Not updated in Customer List, because </b></div>
-	 <?php echo " The Customer Email is invalid <br /> $invalidemail ";  
+	 <?php echo " The Customer Email is invalid <br /> $invalidemail_str";  
 	}
 	?> 
 	
