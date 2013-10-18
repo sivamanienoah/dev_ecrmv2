3ac3ed0957053eb4d640a608abe48a4b1d7b643e
ramakrishnan <ramakrishnan@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2013-10-18 13:34:27 +0000

    Code changed to MVC pattern

--- application/controllers/item_mgmt.php
+++ application/controllers/item_mgmt.php
@@ -5,241 +5,175 @@ class Item_mgmt extends crm_controller {
 	public $cfg;
 	public $userdata;
 	
-	function __construct()
-	{
+	function __construct() {
         parent::__construct();
         $this->login_model->check_login();
+        $this->load->model('item_mgmt_model');
 		$this->userdata = $this->session->userdata('logged_in_user');
     }
     
-    function index($type = 'additional')
-    {
+    /**
+     * Quote Items are listing in dashboard
+     * @param table name - $type
+     */
+    function index($type = 'additional') {
         $data_type = ($type == 'saved') ? 'additional_items' : 'additional_items ';
-
-        $data['table_in_use'] = $type;
-        
+        $data['table_in_use'] = $type;        
         $data['page_heading'] = 'Manage additional items';
-        $data['records'] = array();
+        $data['records'] = array();		
+		$data['categories'] = $this->item_mgmt_model->get_category_list();		
 		
-		$data['categories'] = $this->get_category_list();
-		
-		$c = count($data['categories']);
-		
-		for ($i = 0; $i < $c; $i++)
-		{
-			$this->db->where('item_type', $data['categories'][$i]['cat_id']);
-			$q = $this->db->get($this->cfg['dbpref'] . $data_type);
-			//echo $this->db->last_query();
-			$data['categories'][$i]['records'] = $q->result_array();
+		for ($i = 0; $i < count($data['categories']); $i++) {
+			$data['categories'][$i]['records'] = $this->item_mgmt_model->get_row_bycond($data_type, array('item_type' => $data['categories'][$i]['cat_id']));
 		}
-        //echo "<pre>"; print_r($data); exit;
         $this->load->view('item_mgmt_view', $data);
     }
 	
-	function category_list()
-	{
-		$data['page_heading'] = 'Additional item categories';
-		
-		$data['records'] = $this->get_category_list();
-        
+    /**
+     * Category listing in category dashboad page
+     */
+	function category_list() {
+		$data['page_heading'] = 'Additional item categories';		
+		$data['records'] = $this->item_mgmt_model->get_category_list();        
         $this->load->view('item_mgmt_category_view', $data);
 	}
 	
-	function get_category_list()
-	{
-		$this->db->order_by('cat_id');
-		$q = $this->db->get($this->cfg['dbpref'] . 'additional_cats');
-		return $q->result_array();
-	}
-	
-	function category($update = false, $id = false)
-	{
-		$data['page_heading'] = 'Additional item categories';
-		
+	/**
+	 * Create New category and update category function
+	 * @param if $update is true means, it will update category otherwise insert new category
+	 * @param category id - $id
+	 */
+	function category($update = false, $id = false) {
+		$data['page_heading'] = 'Additional item categories';		
 		$this->load->library('validation');
-        $data = array();
-        
-		$rules['cat_name'] = "trim|required";
-		
-		$this->validation->set_rules($rules);
-		
-		$fields['cat_name'] = 'Category Name';
-		
-		$this->validation->set_fields($fields);
-        
-        $this->validation->set_error_delimiters('<p class="form-error">', '</p>');
-        
-        if ($update == 'update' && preg_match('/^[0-9]+$/', $id) && !isset($_POST['update_item']))
-        {
-            $item_data = $this->db->get_where("{$this->cfg['dbpref']}additional_cats", array('cat_id' => $id));
-            if ($item_data->num_rows() > 0) $customer = $item_data->result_array();
-            if (isset($customer) && is_array($customer) && count($customer) > 0) foreach ($customer[0] as $k => $v)
-            {
-                if (isset($this->validation->$k)) $this->validation->$k = $v;
-            }
+        $data = array();        
+		$rules['cat_name'] = "trim|required";		
+		$this->validation->set_rules($rules);		
+		$fields['cat_name'] = 'Category Name';		
+		$this->validation->set_fields($fields);        
+        $this->validation->set_error_delimiters('<p class="form-error">', '</p>');        
+        if ($update == 'update' && preg_match('/^[0-9]+$/', $id) && !isset($_POST['update_item'])) {
+        	$customer = $this->item_mgmt_model->get_row_bycond('additional_cats', array('cat_id' => $id));
+        	if (isset($customer) && is_array($customer) && count($customer) > 0) {
+        		foreach ($customer[0] as $k => $v) {
+	                if (isset($this->validation->$k)) 
+	                $this->validation->$k = $v;
+        	   	}
+        	}
         }
-		
-		if ($this->validation->run() != false)
-        {
-			
-			// all good
-            foreach($fields as $key => $val)
-            {			
+		if ($this->validation->run() != false) {			
+            foreach($fields as $key => $val) {			
                 $update_data[$key] = $this->input->post($key);				
             }
-            
-            if ($update == 'update' && preg_match('/^[0-9]+$/', $id))
-            {
-                
-                //update
-                $this->db->where('cat_id', $id);
-                 
-                if ($this->db->update("{$this->cfg['dbpref']}additional_cats", $update_data))
-                {
-                    
+            if ($update == 'update' && preg_match('/^[0-9]+$/', $id)) {
+                if ($this->item_mgmt_model->update_row('additional_cats', array('cat_id' => $id), $update_data)) {
                     $this->session->set_flashdata('confirm', array('Item Category Updated!'));
-                    redirect('item_mgmt/category/update/' . $id);
-                    
-                }
-                
-                
-            }
-            else
-            {
-                //insert
-                $this->db->insert("{$this->cfg['dbpref']}additional_cats", $update_data);
+                    //echo $this->db->last_query(); exit;
+                    redirect('item_mgmt/category_list');
+                }    
+            } else {
+                $this->item_mgmt_model->insert_row('additional_cats', $update_data);
                 $this->session->set_flashdata('confirm', array('New Category Added!'));
-                //redirect('item_mgmt/category/update/' . $this->db->insert_id());				
                 redirect('item_mgmt/category_list');				
             }
-			
 		}
-        
         $this->load->view('item_mgmt_category_add', $data);
 	}
-	//mychanges
+	
+	/**
+	 * Check category name is already exits or not.
+	 */
 	function checkcategoryname() {
-		$catname = $_POST['category'];
-		$cat_up = $_POST['cat_up'];
-		//echo $cat_up; exit;
-		if($cat_up == 'undefined') {
-			$this->db->where('cat_name', $catname);
-			$query = $this->db->get($this->cfg['dbpref'].'additional_cats')->num_rows();
+		$catname = $this->input->post('category');
+		$cat_up = $this->input->post('cat_up');
+		if(empty($cat_up)) {
+			$res = $this->item_mgmt_model->get_row_bycond('additional_cats', array('cat_name' =>$catname));
 		} else {
-			$this->db->where('cat_name', $catname);
-			$this->db->where('cat_id !=', $cat_up);
-			$query = $this->db->get($this->cfg['dbpref'].'additional_cats')->num_rows();
+			$cond = array('cat_name' =>$catname, 'cat_id !=' => $cat_up);
+			$res = $this->item_mgmt_model->get_row_bycond('additional_cats', $cond);
 		}
-		//echo $this->db->last_query();
-		if($query == 0 ) 
-			echo 'success';
+		if(empty($res)) 
+		echo json_encode('success');
 		else 
-			echo 'fail';
-			
-		/*echo $catname = $_POST['catname'];
-		$query = $this->db->query("select cat_name from {$this->cfg['dbpref']}additional_cats where cat_name='".$catname."'");
-		if($query->num_rows() > 0) { 
-			$json['msg'] = 'fail';
-		} else {
-			$json['msg'] = 'success';
-		}
-		echo json_encode($json); exit; */
+		echo json_encode('fail');
+		exit;
 	}
     
-    function add($update = false, $id = false, $type = 'additional')
-    {
-       
-        $this->load->library('validation');
-        $data = array();
-        
+	/**
+	 * Add new quote item here.
+	 * @param $update - update quote item if already exits.
+	 * @param $id - quote item id
+	 * @param $type - table name
+	 */
+    function add($update = false, $id = false, $type = 'additional') {
+
+    	$this->load->library('validation');
+        $data = array();        
         $rules['item_price'] = "trim|required|numeric";
 		$rules['item_desc'] = "required";
-		$rules['item_type'] = "required";
-		
-		$this->validation->set_rules($rules);
-		
+		$rules['item_type'] = "required";		
+		$this->validation->set_rules($rules);		
 		$fields['item_price'] = "item Price";
 		$fields['item_desc'] = 'Item Details';
 		$fields['item_type'] = 'Item Category';
-		
-		$this->validation->set_fields($fields);
-        
+		$this->validation->set_fields($fields);        
         $this->validation->set_error_delimiters('<p class="form-error">', '</p>');
 		
-        $data['categories'] = $this->get_category_list();
-        
+        $data['categories'] = $this->item_mgmt_model->get_category_list();        
 		$table_type = 'additional';
         
-        if ($update == 'update' && preg_match('/^[0-9]+$/', $id) && !isset($_POST['update_item']))
-        {
-            $item_data = $this->db->get_where("{$this->cfg['dbpref']}_" . $table_type . 'items', array('itemid' => $id));
-            if ($item_data->num_rows() > 0) $customer = $item_data->result_array();
-            if (isset($customer) && is_array($customer) && count($customer) > 0) foreach ($customer[0] as $k => $v)
-            {
+        if ($update == 'update' && preg_match('/^[0-9]+$/', $id) && !isset($_POST['update_item'])) {
+        	$customer = $this->item_mgmt_model->get_row_bycond('additional_items', array('itemid' =>$id));
+            if (isset($customer) && is_array($customer) && count($customer) > 0) 
+            foreach ($customer[0] as $k => $v) {
                 if (isset($this->validation->$k)) $this->validation->$k = $v;
             }
         }
 		
-		if ($this->validation->run() != false)
-        {
-			
-			// all good
-            foreach($fields as $key => $val)
-            {
+		if ($this->validation->run() != false) {
+            foreach($fields as $key => $val) {
                 $update_data[$key] = $this->input->post($key);
             }
-            
-            if ($update == 'update' && preg_match('/^[0-9]+$/', $id))
-            {
-                
-                //update
-                $this->db->where('itemid', $id);
-                 
-                if ($this->db->update("{$this->cfg['dbpref']}_" . $table_type . 'items', $update_data))
-                {
-                    
+            if ($update == 'update' && preg_match('/^[0-9]+$/', $id)) {        
+                if ($this->item_mgmt_model->update_row('additional_items', array('itemid' => $id), $update_data)) {
                     $this->session->set_flashdata('confirm', array('Item Details Updated!'));
                     redirect('item_mgmt/add/update/' . $id . '/' . $type);
-                    
                 }
-                
-                
-            }
-            else
-            {
-                
-                //insert
-                $this->db->insert("{$this->cfg['dbpref']}_" . $table_type . 'items', $update_data);
+            } else {
+            	$this->item_mgmt_model->insert_row('additional_items', $update_data);
                 $this->session->set_flashdata('confirm', array('New Item Added!'));
                 redirect('item_mgmt/add/update/' . $this->db->insert_id() . '/' . $type);
-                
             }
-			
 		}
-        
         $this->load->view('item_mgmt_add', $data);
     }
 	
-	function item_delete($update = false, $id = false, $type = 'additional')
-	{
+    /**
+     * Delete quote item
+     * @param $update
+     * @param $id
+     * @param $type
+     */
+	function item_delete($update = false, $id = false, $type = 'additional') {
 		if ($this->session->userdata('delete')==1){
-			if ($update == 'update' && preg_match('/^[0-9]+$/', $id))
-			{
-				$this->db->delete("{$this->cfg['dbpref']}additional_items", array('itemid' => $id));
+			if ($update == 'update' && preg_match('/^[0-9]+$/', $id)) {
+				$this->item_mgmt_model->delete_row('additional_items', array('itemid' => $id));
 				$this->session->set_flashdata('confirm', array('Item Record Deleted!'));
 				redirect('item_mgmt');
 			}
-		}
-		else {
+		} else {
 			$this->session->set_flashdata('login_errors', array("You have no rights to access this page"));
 			redirect('item_mgmt');
 		}
 	}
 	
-	function delete_category($id = false)
-	{
+	/**
+	 * Delete category from additional cats table
+	 * @param $id
+	 */
+	function delete_category($id = false) {
 		if ($this->session->userdata('delete')==1){
-			$this->db->delete("{$this->cfg['dbpref']}additional_cats", array('cat_id' => $id));
+			$this->item_mgmt_model->delete_row('additional_cats', array('cat_id' => $id));
 			$this->session->set_flashdata('confirm', array('Category Record Deleted!'));
 			redirect('item_mgmt/category_list');
 		}
