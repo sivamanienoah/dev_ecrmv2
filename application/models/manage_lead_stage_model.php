9e0c1318eef87fa704d45eb565697f379d98d39c
ramakrishnan <ramakrishnan@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2013-10-21 13:17:42 +0000

    MVC pattern updated

--- application/models/manage_lead_stage_model.php
+++ application/models/manage_lead_stage_model.php
@@ -17,6 +17,49 @@ class Manage_lead_stage_model extends crm_model {
 		$leadstg =  $query->result_array();
 		return $leadstg;
     }
+    
+	function get_row($table, $cond) {
+    	$res = $this->db->get_where($this->cfg['dbpref'].$table, $cond);
+        return $res->result_array();
+    }
+    
+	function get_num_row($table, $cond) {
+    	$res = $this->db->get_where($this->cfg['dbpref'].$table, $cond);
+        return $res->num_rows();
+    }
+    
+	function get_last_row($table, $limit, $select, $order) {
+    	$this->db->select($select);
+    	$this->db->order_by($order);
+    	if(!empty($limit))
+    	$this->db->limit($limit);
+    	$res = $this->db->get($this->cfg['dbpref'].$table);
+        return $res->result_array();
+    }
+    
+	function update_row($table, $cond, $data) {
+    	$this->db->where($cond);
+		return $this->db->update($this->cfg['dbpref'].$table, $data);
+    }
+    
+	function insert_row($table, $param) {
+    	$this->db->insert($this->cfg['dbpref'].$table, $param);
+    }
+    
+	function delete_row($table, $cond) {
+        $this->db->where($cond);
+        return $this->db->delete($this->cfg['dbpref'].$table);
+    }
+    
+	/*function lead_drag_drop_update($leadst) {
+		$when = '';
+	 	foreach ($leadst as $k => $v) {
+        	$when .= "WHEN ".$v." THEN ".$k." \n";
+        }
+		$this->db->set('sequence', 'CASE WHEN lead_stage_id '.$when. ' ELSE `sequence` END', FALSE);
+		$this->db->update($this->cfg['dbpref'].'crms_lead_stage');
+		echo $this->db->last_query(); exit;
+    }*/
    
 }
 
--- application/models/manage_service_model.php
+++ application/models/manage_service_model.php
@@ -44,7 +44,6 @@ class manage_service_model extends crm_model {
 			$this->db->like('lead_source_name', $search); 
 		}
 		$query = $this->db->get();
-		//echo $this->db->last_query();
 		$leads =  $query->result_array();
 		return $leads;
     }
@@ -57,7 +56,6 @@ class manage_service_model extends crm_model {
 			$this->db->like('expect_worth_name', $search);
 		}
 		$query = $this->db->get();
-		// echo $this->db->last_query();
 		$expect =  $query->result_array();
 		return $expect;
     }
@@ -66,33 +64,27 @@ class manage_service_model extends crm_model {
 		$this->db->select('*');
 		$this->db->from($this->cfg['dbpref'].'currency_all');
 		$query = $this->db->get();
-		//echo $this->db->last_query();
 		$cur =  $query->result_array();
 		return $cur;
     }
 	
 	function insert_new_currency($ins) {
-		// echo "<pre>"; print_r($ins); exit;
 		$this->db->insert("{$this->cfg['dbpref']}" . 'expect_worth', $ins);
 		$last_ins_id = $this->db->insert_id();
 		if (array_key_exists('is_default', $ins)) {
 			$data['is_default'] = 0;
 			$cur_conver['to'] = $last_ins_id;
-			// $this->db->where("expect_worth_id !=", $last_ins_id);
 			$this->db->update($this->cfg['dbpref'].'expect_worth', $data, "expect_worth_id != ".$last_ins_id." ");
-			// $this->db->update($this->cfg['dbpref'].'currency_rate', $cur_conver);
 			$this->db->truncate($this->cfg['dbpref'].'currency_rate');
 		}
     }
 	
 	function updt_exist_currency($updt, $id) {
-		// echo "<pre>"; print_r($updt); exit;
 		$res = $this->db->update($this->cfg['dbpref'].'expect_worth', $updt, "expect_worth_id = ".$id." ");
 		if (array_key_exists('is_default', $updt)) {
 			$data[is_default] = 0;
 			$cur_conver['to'] = $id;
 			$this->db->update($this->cfg['dbpref'].'expect_worth', $data, "expect_worth_id != ".$id." ");
-			// $this->db->update($this->cfg['dbpref'].'currency_rate', $cur_conver);
 			$this->db->truncate($this->cfg['dbpref'].'currency_rate');
 		}
 		return $res;
@@ -103,11 +95,34 @@ class manage_service_model extends crm_model {
 		$this->db->from($this->cfg['dbpref'].'currency_all');
 		$this->db->where('cur_id', $id);
 		$query = $this->db->get();
-		// echo $this->db->last_query(); exit;
 		$res =  $query->row_array();
 		return $res;
     }
    
+	function get_row($table, $cond) {
+    	$res = $this->db->get_where($this->cfg['dbpref'].$table, $cond);
+        return $res->result_array();
+    }
+	
+    function get_num_row($table, $cond) {
+    	$res = $this->db->get_where($this->cfg['dbpref'].$table, $cond);
+        return $res->num_rows();
+    }
+    
+    function update_row($table, $cond, $data) {
+    	$this->db->where($cond);
+		return $this->db->update($this->cfg['dbpref'].$table, $data);
+    }
+    
+	function insert_row($table, $param) {
+    	$this->db->insert($this->cfg['dbpref'].$table, $param);
+    }
+    
+	function delete_row($table, $cond) {
+        $this->db->where($cond);
+        return $this->db->delete($this->cfg['dbpref'].$table);
+    }
+    
 }
 
 ?>
