efc9e1e10db31dda17e46f5bb5719552c05fdf44
sriram <sriram@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2013-11-07 05:35:25 +0000

    currency conversion api changed from igoogle to google

--- application/helpers/custom_helper.php
+++ application/helpers/custom_helper.php
@@ -14,7 +14,7 @@ if ( ! function_exists('get_default_currency'))
 			return false;
 		else 
 			$res = $query->row_array();
-		// echo $CI->db->last_query(); exit;
+			// echo $CI->db->last_query(); exit;
 		return $res;
 	}
 }
@@ -43,7 +43,7 @@ function currency_convert()
 			$amount = urlencode($amount);
 			$from_Currency = urlencode($cur->expect_worth_name);
 			$to_Currency = urlencode($to_Currency);
-			$url = "http://www.google.com/ig/calculator?hl=en&q=$amount$from_Currency=?$to_Currency";
+			/* $url = "http://www.google.com/ig/calculator?hl=en&q=$amount$from_Currency=?$to_Currency";
 			$ch = curl_init();
 			$timeout = 0;
 			curl_setopt ($ch, CURLOPT_URL, $url);
@@ -54,24 +54,34 @@ function currency_convert()
 			curl_close($ch);
 			
 			$data = explode('"', $rawdata);
-			$data = explode(' ', $data['3']);
-			$var = $data['0'];
-			$conversion_value = round($var,3);
+			$data = explode(' ', $data['3']); 
+			$var = $data['0']; */
+
+			$url = "https://www.google.com/finance/converter?a=$amount&from=$from_Currency&to=$to_Currency";
+
+			$amount = urlencode($amount);
+			$from_Currency = urlencode($from_Currency);
+			$to_Currency = urlencode($to_Currency);
+			$get = file_get_contents("https://www.google.com/finance/converter?a=$amount&from=$from_Currency&to=$to_Currency");
+			$get = explode("<span class=bld>",$get);
+			$get = explode("</span>",$get[1]);  
+			$converted_amount = preg_replace("/[^0-9\.]/", null, $get[0]);
+
+			$conversion_value = round($converted_amount, 3);
 			
 			$CI->db->where('from', $cur->expect_worth_id);
 			$CI->db->where('to', $to_Currency_id);
 			$query1 = $CI->db->get($cfg['dbpref'].'currency_rate');
 			$res_num = $query1->num_rows();
 			
-			if($res_num>0)
-			{
+			if($res_num>0) {
 				if(!empty($conversion_value)){
 					$CI->db->where('from', $cur->expect_worth_id);
 					$CI->db->where('to', $to_Currency_id);
 					$CI->db->set('value', $conversion_value);
 					$CI->db->update($cfg['dbpref'].'currency_rate');
 				}
-			}else{
+			} else {
 				if(!empty($conversion_value)){
 					$CI->db->set('from', $cur->expect_worth_id);
 					$CI->db->set('to', $to_Currency_id);
@@ -79,7 +89,6 @@ function currency_convert()
 					$CI->db->insert($cfg['dbpref'].'currency_rate');
 				}				
 			}
-			
 		}	
 	}
 }
