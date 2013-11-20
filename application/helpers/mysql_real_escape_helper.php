a644287a79e05ed3c23a518015e38133d723b48c
sriram <sriram@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2013-11-20 09:34:12 +0000

    removed redundancy

--- application/helpers/mysql_real_escape_helper.php
+++ application/helpers/mysql_real_escape_helper.php
@@ -38,59 +38,18 @@
 if ( ! function_exists('real_escape_array'))
 {
 	function real_escape_array($data = array())
-	{
-	
+	{	
 		$post_data_arr = array();
 		if(sizeof($data)>0){
 			foreach($data as $key=>$value){
-			
 				if(is_array($value) && sizeof($value)>0){
-					$post_data_arr[$key] = multi_dimention_firstlevel($value);
+					$post_data_arr[$key] = real_escape_array($value);
 				}else{
 					$post_data_arr[$key] = mysql_real_escape_string($value);
 				}
 			}
 		}
 		return $post_data_arr;
-	}
-	
-	/*
-	*@Multi Dimention Array First Level 
-	*/
-	function multi_dimention_firstlevel($data=array()){
-	
-		$post_firstlevel_data = array();
-	
-		if(sizeof($data)>0){
-			foreach($data as $key=>$value){
-			    if(is_array($value) && sizeof($data)>0){
-					$post_firstlevel_data[$key] = multi_dimention_nextlevel($value);
-				}else{
-					$post_firstlevel_data[$key] = mysql_real_escape_string($value);
-				}
-			}
-	    }
-		return $post_firstlevel_data;
-	}
-
-
-	/*
-	*@Multi Dimention Array Next Level 
-	*/
-	function multi_dimention_nextlevel($data=array()){
-	
-		$post_nextlevel_data = array();
-	
-		if(sizeof($data)>0){
-			foreach($data as $key=>$value){
-			    if(is_array($value) && sizeof($data)>0){
-					$post_nextlevel_data[$key] = multi_dimention_firstlevel($value);
-				}else{
-					$post_nextlevel_data[$key] = mysql_real_escape_string($value);
-				}
-			}
-	    }
-		return $post_nextlevel_data;
 	}
 	
 }
