79c642b6cfc10f68eb4b96e9715fea83ed2b9d73
sriram <sriram@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2013-11-25 09:24:37 +0000

    removed empty space

--- assets/js/item_mgmt/item_mgmt_category_add.js
+++ assets/js/item_mgmt/item_mgmt_category_add.js
@@ -2,32 +2,32 @@
  *@Item Management Category Jquery
 */
 
-	$(document).ready(function() {
-		$('.errmsg').hide();
-	});
+$(document).ready(function() {
+	$('.errmsg').hide();
+});
 
-	function valid() {
-		var catname 			= $("#cat_name").val();
-		var catupdt 			= $("#cat_updt").val();
-		var params 				= {category: catname, cat_up: catupdt};
-		params[csrf_token_name] = csrf_hash_token;
-		
-		$.ajax({
-			url: "item_mgmt/checkcategoryname",
-			data: params,
-			type: "POST",
-			dataType: 'json',
-			success: function(data){
-				if(data == 'fail') {
-					$('.errmsg').show();
-					return false;
-				} else {
-					document.formone.submit();
-				}
-			}		
-		});
-		return false;
-	}
+function valid() {
+	var catname 			= $("#cat_name").val();
+	var catupdt 			= $("#cat_updt").val();
+	var params 				= {category: catname, cat_up: catupdt};
+	params[csrf_token_name] = csrf_hash_token;
+	
+	$.ajax({
+		url: "item_mgmt/checkcategoryname",
+		data: params,
+		type: "POST",
+		dataType: 'json',
+		success: function(data){
+			if(data == 'fail') {
+				$('.errmsg').show();
+				return false;
+			} else {
+				document.formone.submit();
+			}
+		}		
+	});
+	return false;
+}
 
 
 /////////////////
