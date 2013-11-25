d9c3dc75aa750c59260bd4a72e66a9dd7851ccdb
sriram <sriram@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2013-11-25 14:16:02 +0000

    for checking duplicate condition added.

--- assets/js/role/add_view.js
+++ assets/js/role/add_view.js
@@ -12,5 +12,37 @@ $(document).ready(function() {
 		}
 	});
 });
+
+function chk_role_name() {
+	$('#role_msg').empty();
+	var role_name 			= $("#role_name").val();
+	var role_id_hidden     	= $("#role_id_hidden").val();
+	var type		        = 'roles';
+	var params 				= {name: role_name, id: role_id_hidden, type: type};
+	params[csrf_token_name] = csrf_hash_token;
+	
+	if (role_name == "") {
+		$('#role_msg').show();
+		$('#role_msg').append("<span class='ajx_failure_msg'>Role Name Required.</span>");
+		return false;
+	} else {
+		$.ajax({
+			url: "role/chk_role_duplicate",
+			data: params,
+			type: "POST",
+			dataType: 'json',
+			success: function(data) {
+				if(data == 'fail') {
+					$('#role_msg').show();
+					$('#role_msg').append("<span class='ajx_failure_msg'>Role Name Already Exists.</span>");
+					return false;
+				} else {
+					document.add_role.submit();
+				}
+			}		
+		});
+	}
+	return false;
+}
 	
 //////////////////////////////////////////////////////////////////// end ///////////////////////////////////////////////////
