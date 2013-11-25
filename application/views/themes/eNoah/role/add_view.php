4c98c5430bd9acee2f073a83b0005355a6c39305
sriram <sriram@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2013-11-25 14:16:13 +0000

    for checking duplicate condition added.

--- application/views/themes/eNoah/role/add_view.php
+++ application/views/themes/eNoah/role/add_view.php
@@ -2,34 +2,39 @@
 <script type="text/javascript" src="assets/js/jq.livequery.min.js"></script>
 <script type="text/javascript" src="assets/js/role/add_view.js"></script>
 <div id="content">
- 
+
     <div class="inner"> 
 	<?php if(($this->session->userdata('add')==1 && ($this->uri->segment(3) != 'update')) || (($this->uri->segment(3) == 'update') && is_numeric($this->uri->segment(4)) && ($this->session->userdata('edit')==1))) { ?>
-    	<form action="<?php echo  $this->uri->uri_string() ?>" method="post">
+    	<form action="<?php echo  $this->uri->uri_string() ?>" method="post" name="add_role" onsubmit="return chk_role_name();">
 		
 		   <input id="token" type="hidden" name="<?php echo $this->security->get_csrf_token_name(); ?>" value="<?php echo $this->security->get_csrf_hash(); ?>" />
 		
             <h2><?php echo  ($this->uri->segment(3) == 'update' && is_numeric($this->uri->segment(4))) ? 'Update' : 'New' ?> Role Details</h2>
             <?php if ($this->validation->error_string != '') { ?>
             <div class="form_error">
-                <?php echo  $this->validation->error_string ?>
+                <?php echo $this->validation->error_string ?>
             </div>
             <?php } ?>
             <p>All mandatory fields marked * must be filled in correctly.</p>
 			<table class="layout">
 				<tr>
-					<td width="100">Role name:</td>
-					<td width="240">
-						<input type="text" name="name" value="<?php echo  $this->validation->name ?>" class="textfield width200px required" /> *
-					</td>					 
+					<td>Role name:</td>
+					<td>
+						<input type="text" name="name" id="role_name" value="<?php echo $this->validation->name ?>" class="textfield width200px required" /> *
+						<?php if ($this->uri->segment(3) == 'update') { ?>
+							<input type="hidden" id="role_id_hidden" name="role_id_hidden" value="<?php echo $this->uri->segment(4); ?>" />
+						<?php } ?>
+						
+					</td>
+					<td><div id="role_msg"> </div></td>					
 				</tr>			 
 				<tr> 
 					<td>Modules:</td>
-					<td><?php echo $pageTree; ?></td>
+					<td colspan="2"><?php echo $pageTree; ?></td>
 				</tr>			 			   
 				<tr>
 					<td>Inactive Role:</td>
-					<td colspan="3">
+					<td colspan="2">
 						<input type="checkbox" name="inactive" value="1" <?php if ($this->validation->inactive == 1) echo ' checked="checked"' ?>
 						<?php if ($cb_status != 0) echo 'disabled="disabled"' ?>> 
 						<?php if ($cb_status != 0) echo "One or more User currently assigned for this Role. This cannot be made Inactive."; ?>
@@ -39,24 +44,13 @@
 				</tr>
                 <tr>
 					<td>&nbsp;</td>
-					<?php if (isset($this_user) && $userdata['userid'] == $this_user) { ?>
-					<td colspan="3">
-						Active Role cannot be modified! Please use my account to update your details.
-					</td>
-					<?php } else if (isset($this_user_level) && $userdata['level'] >= $this_user_level && $userdata['level'] != 0) { ?>
-					<td colspan="3">
-						Your Role level cannot updated similar levels or levels above you.
-					</td>
-					<?php } else { ?>
 					<td>
                         <div class="buttons">
 							<button type="submit" name="update_user" class="positive">
-								
 								<?php echo  ($this->uri->segment(3) == 'update' && is_numeric($this->uri->segment(4))) ? 'Update' : 'Add' ?> Role
 							</button>
 						</div>
                     </td>                     
-					<?php } ?>
 				</tr>
             </table>
 		</form>
