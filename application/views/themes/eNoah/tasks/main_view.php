3f78adb908b24d628f973e5400301462b431cf4c
sriram <sriram@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2013-12-04 12:50:21 +0000

    js moved to new file

--- application/views/themes/eNoah/tasks/main_view.php
+++ application/views/themes/eNoah/tasks/main_view.php
@@ -307,94 +307,11 @@ ol#pagination{overflow:hidden; padding-top:50px; padding-left:15px;}
 	
 </div>
 <script type="text/javascript">
-$(function(){
-	$('.all-tasks').load('tasks/index/extend #task-page .task-contents', {'<?php echo $this->security->get_csrf_token_name(); ?>':'<?php echo $this->security->get_csrf_hash(); ?>'}, loadEditTables);
-	$('#set-job-task .pick-date, #edit-job-task .pick-date').datepicker({dateFormat: 'dd-mm-yy', minDate: '0', maxDate: '+6M'});
-	$('#search-job-task .pick-date').datepicker({dateFormat: 'dd-mm-yy'});
-	
-	$('#task_search_user').val('<?php echo $userdata['userid']; ?>');
-	/* job tasks character limit */
-	$('#job-task-desc').keyup(function(){
-		var desc_len = $(this).val();
-		if (desc_len.length > 1000) {
-			$(this).focus().val(desc_len.substring(0, 1000));
-		}
-		var remain_len = 1000 - desc_len.length;
-		if (remain_len < 0) remain_len = 0;
-		$('#task-desc-countdown').text(remain_len);
-	});
-	$('#edit-job-task-desc').keyup(function(){
-		var desc_len = $(this).val();
-		
-		if (desc_len.length > 1000) {
-			$(this).focus().val(desc_len.substring(0, 1000));
-		}
-		
-		var remain_len = 1000 - desc_len.length;
-		if (remain_len < 0) remain_len = 0;
-		
-		$('#edit-task-desc-countdown').text(remain_len);
-	});
-});
-
-function searchTasks(){
-	$('.tasks-search .search-results').empty().html('Loading...');
-	$.post('tasks/search',$('#search-job-task').serialize(),function(data) {
-		$('.tasks-search').find('.search-results').remove();
-		$('.all-tasks').hide();
-		$('.tasks-search').append(data);
-	});
-	return false;
-}
-function searchtodayTasks(){
-	$('.tasks-search .search-results').empty().html('Loading...');
-	$.post('tasks/search','task_search_user='+$('#task_search_user').val()+'&task_search_start_date='+$('#hided').val()+'&task_search_end_date='+$('#hided').val(),function(data) {
-		$('.tasks-search').find('.search-results').remove();
-		$('.all-tasks').hide();
-		$('.tasks-search').append(data);
-	});
-	return false;
-}
-function loadEditTables(){
-	$('#jv-tab-4').block({
-            message:'<h4>Processing</h4><img src="assets/img/ajax-loader.gif" />',
-			css: {background:'#666', border: '2px solid #999', padding:'4px', height:'35px', color:'#333'}
-        });
-		
-	var taskids = [];
-	$('td.random-task').each(function(){	
-		taskids.push($(this).attr('rel'));
-			$(this).append('<div class="buttons" style="display:none"> \
-								<button style="margin:0!important;background: none repeat scroll 0 0 transparent;" type="submit" onclick="openEditTask(\'' + $(this).attr('rel') + '\', \'random\'); return false;\">Edit | </button> \
-								<button style="margin:0!important;background: none repeat scroll 0 0 transparent;" type="submit" onclick="setTaskStatus(\'' + $(this).attr('rel') + '\', \'complete\'); return false;">Approve | </button> \
-								<button style="margin:0!important;background: none repeat scroll 0 0 transparent;" type="submit" onclick="setTaskStatus(\'' + $(this).attr('rel') + '\', \'delete\'); return false;">Delete</button> \
-							</div>');	
-		
-	});
-	$('td.newrandom-task').each(function(){
-		taskids.push($(this).attr('rel'));
-			$(this).append('<div class="buttons" style="display:none"> \
-								<button style="margin:0!important;background: none repeat scroll 0 0 transparent;" type="submit" onclick="openEditTask(\'' + $(this).attr('rel') + '\', \'random\'); return false;\">Edit</button> \
-							</div>');
-	});
-	if (taskids.length < 1)	{
-		$('#jv-tab-4').unblock();
-		return;
-	}
-	$.post('ajax/request/get_random_tasks',{'id_set': taskids.join(','),'<?php echo $this->security->get_csrf_token_name(); ?>':'<?php echo $this->security->get_csrf_hash(); ?>'},function(data){
-		if (data != '')	{
-			$('form.random-task-tables').html(data);
-		}
-		$('#jv-tab-4').unblock();
-	});
-}
-
-<?php if(isset($_GET['type'])&&$_GET['type']=='random'){ ?>
-	openEditTask('68','random');  
-<?php } ?>
-
-
+	var task_userid = '<?php echo $userdata['userid'] ?>';
+	var get_type 	= '<?php echo $_GET['type'] ?>';
+	var get_id      = '<?php echo $_GET['id'] ?>';
 </script>
+<script type="text/javascript" src="assets/js/tasks/main_view.js"></script>
 <?php
 require theme_url() . '/tpl/footer.php';
 ob_end_flush();
