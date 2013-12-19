5eef6cb4aeeda71ed037b37d031dc4d00e6e4102
sriram <sriram@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2013-12-19 05:17:09 +0000

    alignment

--- application/views/themes/eNoah/user/add_view.php
+++ application/views/themes/eNoah/user/add_view.php
@@ -4,19 +4,23 @@
 
 <div id="content">
     <div class="inner">
-	<?php if(($this->session->userdata('add')==1 && $this->uri->segment(3) != 'update')|| (($this->uri->segment(3) == 'update' && is_numeric($this->uri->segment(4))) && ($this->session->userdata('edit')==1))){ ?>
-		
-			<form action="<?php echo  $this->uri->uri_string() ?>" method="post" id="frm">
-		
+	<?php 
+	if(($this->session->userdata('add')==1 && $this->uri->segment(3) != 'update')|| (($this->uri->segment(3) == 'update' && is_numeric($this->uri->segment(4))) && ($this->session->userdata('edit')==1))) { 
+	?>
+		<form action="<?php echo  $this->uri->uri_string() ?>" method="post" id="frm">
 			<input id="token" type="hidden" name="<?php echo $this->security->get_csrf_token_name(); ?>" value="<?php echo $this->security->get_csrf_hash(); ?>" />
 			
             <h2><?php echo  ($this->uri->segment(3) == 'update' && is_numeric($this->uri->segment(4))) ? 'Update' : 'New' ?> User Details</h2>
-            <?php if ($this->validation->error_string != '') { ?>
-            <div class="form_error">
-                <?php echo  $this->validation->error_string ?>
-            </div>
-            <?php } ?>
-            <!--<p>All mandatory fields marked * must be filled in correctly.</p>-->
+            <?php 
+			if ($this->validation->error_string != '') { 
+			?>
+				<div class="form_error">
+					<?php echo $this->validation->error_string; ?>
+				</div>
+            <?php 
+			} 
+			?>
+
 			<table class="layout">
 				<tr>
 					<td width="100">First Name: *</td>
@@ -36,7 +40,7 @@
 				</tr>
 				<tr>
 					<td>Email: *</td>
-					<td><input type="text" id="email" name="email" value="<?php echo $this->validation->email ?>" class="textfield width200px" /><br/> 
+					<td><input type="text" id="email" name="email" value="<?php echo $this->validation->email ?>" class="textfield width200px" autocomplete="off"/><br/> 
 					<span class="error" style="color:red;" id="error4">required</span>
 					<span class="error" style="color:red;" id="notvalid">Not a valid e-mail address</span>
 					<span class="checkUser" style="color:green">Email Available.</span>
@@ -58,68 +62,70 @@
                         </select> 
 						<div class="error" style="color:red;" id="error3">required</div><input type="hidden" value="0" id="role_change_mail" name="role_change_mail"/>
 						<script>
-						$('#role_id').change(function() {
-							var assign_mail = $('#role_id').val();
-							//alert(assign_mail);
-							$('#role_change_mail').val(assign_mail);
-						});
+							$('#role_id').change(function() {
+								var assign_mail = $('#role_id').val();
+								//alert(assign_mail);
+								$('#role_change_mail').val(assign_mail);
+							});
 						</script>
 					</td>
 				</tr>
-				
 				<tr>
 					<td>Password: *</td>
-					<td><input type="password" id="password" name="password" value="" class="textfield width200px" />
+					<td><input type="password" id="password" name="password" value="" class="textfield width200px" autocomplete="off"/>
 						<?php if ($this->uri->segment(3) != 'update') { ?>
 							<div class="error" style="color:red;" id="error5">required</div>
 						<?php } ?>
-					<?php //if ($this->uri->segment(3) != 'update') echo ' *' ?>
 					</td>
                     <td>&nbsp;</td>
                     <td>
-						<?php if (($this->uri->segment(3) == 'update' && is_numeric($this->uri->segment(4))) && ($this->session->userdata('edit')==1)) { ?>
-						
+						<?php 
+						if (($this->uri->segment(3) == 'update' && is_numeric($this->uri->segment(4))) && ($this->session->userdata('edit')==1)) { 
+						?>
 							<input type="checkbox" name="update_password" value="1" /> Update Password?
-						<?php } else { ?>
+						<?php 
+						} else { 
+						?>
 							&nbsp; <input type="hidden" name="new_user" value="1" />
-						<?php } ?>
+						<?php 
+						} 
+						?>
 					</td>					
 				</tr>
 				<tr>
 				<!-- Levels and region settings starts here -->
-				<td>Level: *</td>
+					<td>Level: *</td>
 					<td>
-					<?php if($this->uri->segment(3) != 'update') { ?>
-                        <select id="level_id" name="level" class="textfield width200px">
-                            <option value="">Please Select</option>
-							<?php
-							foreach ($levels as $val) {							
-								?>
-								<option value="<?php echo $val['level_id']; ?>"<?php echo  ($this->validation->level == $val['level_id']) ? ' selected="selected"' : '' ?>><?php echo  $val['level_name']; ?></option>
-							<?php
-								
-							}
-							?>
-                        </select> <br/>
-						<div class="error" style="color:red;" id="error6">required</div><br/> <br/> 
-						<div class="level-message"></div>
-						
-						<?php } else { ?>
-						<select id="level_id" name="level" class="textfield width200px">
-                            <option value="">Please Select</option>
-							<?php
-							foreach ($levels as $val) {							
-								?>
-								<option value="<?php echo $val['level_id']; ?>"<?php echo  ($this->validation->level == $val['level_id']) ? ' selected="selected"' : '' ?>><?php echo  $val['level_name']; ?></option>
-							<?php
-								
-							}
-							?>
-                        </select><br/> <div class="error" style="color:red;" id="error6">required</div><br/><br/> <div class="level-message"></div>
-						<?php } ?>						
+						<?php 
+						if($this->uri->segment(3) != 'update') { 
+						?>
+							<select id="level_id" name="level" class="textfield width200px">
+								<option value="">Please Select</option>
+								<?php foreach ($levels as $val) { ?>
+									<option value="<?php echo $val['level_id']; ?>"<?php echo  ($this->validation->level == $val['level_id']) ? ' selected="selected"' : '' ?>><?php echo  $val['level_name']; ?></option>
+								<?php } ?>
+							</select> 
+							<br/>
+							<div class="error" style="color:red;" id="error6">required</div><br/> <br/> 
+							<div class="level-message"></div>
+						<?php 
+						} else { 
+						?>
+							<select id="level_id" name="level" class="textfield width200px">
+								<option value="">Please Select</option>
+								<?php foreach ($levels as $val) { ?>
+									<option value="<?php echo $val['level_id']; ?>"<?php echo  ($this->validation->level == $val['level_id']) ? ' selected="selected"' : '' ?>><?php echo  $val['level_name']; ?></option>
+								<?php } ?>
+							</select><br/> <div class="error" style="color:red;" id="error6">required</div><br/><br/> <div class="level-message"></div>
+						<?php 
+						} 
+						?>						
 						<input type="hidden" value="0" id="level_change_mail" name="level_change_mail"/>
-				<!-- Levels and region settings ends here -->	
-					<?php if (($this->uri->segment(3) == 'update' && is_numeric($this->uri->segment(4))) && ($this->session->userdata('edit')==1)) { ?>
+					</td>
+					<!-- Levels and region settings ends here -->	
+					<?php 
+					if (($this->uri->segment(3) == 'update' && is_numeric($this->uri->segment(4))) && ($this->session->userdata('edit')==1)) { 
+					?>
 					<td>Inactive User:</td>
 					<td colspan="3">
 						<input type="checkbox" name="inactive" value="1" <?php if ($this->validation->inactive == 1) echo ' checked="checked"' ?>
@@ -128,47 +134,48 @@
 						<?php if (($this->validation->inactive == 0) && ($cb_status == 0)) echo "Check if the User need to be Inactive."; ?>
 						<?php if ($this->validation->inactive != 0) echo "Uncheck if the User need to be Active."; ?>						
 					</td>
-					<?php } ?>
+					<?php 
+					} 
+					?>
 				</tr>
 				<tr>
-				
 				<td colspan="5">
 					<div class="container-region" style="float:left;display:none;">
-							<table border="0" cellpadding="0" cellspacing="0" class="data-tabl-dupl">
-								<thead>
-										<tr>
-											<th><div class="select-region" style="display:none">Select Region</div></th>
-											<th><div class="select-country" style="display:none">Select Country</div></th>
-											<th><div class="select-state" style="display:none">Select State</div></th>
-											<th><div class="select-location" style="display:none">Select Location</div></th>
-										</tr>
-								</thead>
-								<tbody>
+						<table border="0" cellpadding="0" cellspacing="0" class="data-tabl-dupl">
+							<thead>
 									<tr>
-										<td>
-											<div class="region-box" style="display:none">
-												<select id="region_load" class="required" name="region[]" multiple="multiple">
-											</div>
-										</td>
-										<td id="country_row">
-											<div class="country-box" style="display:none">
-											<select name="country[]" class="required" id="country_load" multiple="multiple"><option value="">Select</option></select>
-											</div>
-										</td>
-										<td id="state_row">
-											<div class="state-box" style="display:none">
-											<select name="state[]" class="required" id="state_load" multiple="multiple"><option value="">Select</option></select>
-											</div>
-										</td>
-										<td id="location_row">
-											<div class="location-box" style="display:none">
-											<select name="location[]" class="required" id="location_load" multiple="multiple"><option value="">Select</option></select>
-											</div>
-										</td>
+										<th><div class="select-region" style="display:none">Select Region</div></th>
+										<th><div class="select-country" style="display:none">Select Country</div></th>
+										<th><div class="select-state" style="display:none">Select State</div></th>
+										<th><div class="select-location" style="display:none">Select Location</div></th>
 									</tr>
-								</tbody>
-							</table>
-						</div>
+							</thead>
+							<tbody>
+								<tr>
+									<td>
+										<div class="region-box" style="display:none">
+											<select id="region_load" class="required" name="region[]" multiple="multiple">
+										</div>
+									</td>
+									<td id="country_row">
+										<div class="country-box" style="display:none">
+										<select name="country[]" class="required" id="country_load" multiple="multiple"><option value="">Select</option></select>
+										</div>
+									</td>
+									<td id="state_row">
+										<div class="state-box" style="display:none">
+										<select name="state[]" class="required" id="state_load" multiple="multiple"><option value="">Select</option></select>
+										</div>
+									</td>
+									<td id="location_row">
+										<div class="location-box" style="display:none">
+										<select name="location[]" class="required" id="location_load" multiple="multiple"><option value="">Select</option></select>
+										</div>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
 				</td>
 				</tr>
 				<tr><td><td>&nbsp;</td></td></tr>				
@@ -186,14 +193,14 @@
 							</button>
                         </div>
                     </td>
-                    
-					<?php //} ?>
 				</tr>
             </table>
 		</form>
-		<?php } else {
+		<?php 
+		} else {
 			echo "You have no rights to access this page";
-		} ?>
+		} 
+		?>
 	</div>
 </div>
 <?php require (theme_url(). '/tpl/footer.php'); ?>
