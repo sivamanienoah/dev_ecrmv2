28977e98649915e029c4728ff7d00fa1542a57d0
sriram <sriram@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2014-07-01 06:24:34 +0000

    currency conversion changes

--- application/helpers/custom_helper.php
+++ application/helpers/custom_helper.php
@@ -21,57 +21,64 @@ if ( ! function_exists('get_default_currency'))
 
 function currency_convert()
 {
-	if (get_default_currency()) {
+	/* if (get_default_currency()) {
 		$default_currency = get_default_currency();
 		$to_Currency = $default_currency['expect_worth_name'];
 		$to_Currency_id = $default_currency['expect_worth_id'];
 	} else {
 		$to_Currency = 'USD';
 		$to_Currency_id = 1;
-	}
+	} */
 	
 	$CI  = get_instance();
 	$cfg = $CI->config->item('crm'); /// load config
 	
 	$query = $CI->db->get($CI->cfg['dbpref'].'expect_worth');
 	$res = $query->result();
+
 	if(!empty($res)){
-		foreach ($res as $cur)
-		{			
-			// $to_Currency = 'USD';
-			$amount = 1;
-			$amount = urlencode($amount);
-			$from_Currency = urlencode($cur->expect_worth_name);
-			$to_Currency = urlencode($to_Currency);
-			/* $url = "http://www.google.com/ig/calculator?hl=en&q=$amount$from_Currency=?$to_Currency";
-			$ch = curl_init();
-			$timeout = 0;
-			curl_setopt ($ch, CURLOPT_URL, $url);
-			curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
-			curl_setopt($ch,  CURLOPT_USERAGENT , "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1)");
-			curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-			$rawdata = curl_exec($ch);
-			curl_close($ch);
-			
-			$data = explode('"', $rawdata);
-			$data = explode(' ', $data['3']); 
-			$var = $data['0']; */
-
-			$url = "https://www.google.com/finance/converter?a=$amount&from=$from_Currency&to=$to_Currency";
-
-			$amount = urlencode($amount);
-			$from_Currency = urlencode($from_Currency);
-			$to_Currency = urlencode($to_Currency);
-			$get = file_get_contents("https://www.google.com/finance/converter?a=$amount&from=$from_Currency&to=$to_Currency");
-			$get = explode("<span class=bld>",$get);
-			$get = explode("</span>",$get[1]);  
-			$converted_amount = preg_replace("/[^0-9\.]/", null, $get[0]);
-			
-			$conversion_value = round($converted_amount, 3);
-			updt_currency($cur->expect_worth_id, $to_Currency_id, $conversion_value);
-			
+		foreach ($res as $curren)
+		{
+			foreach ($res as $cur)
+			{
+				// $to_Currency = 'USD';
+				$amount = 1;
+				$amount = urlencode($amount);
+				$from_Currency = urlencode($curren->expect_worth_name);
+				$from_Currency_id = urlencode($curren->expect_worth_id);
+				// $to_Currency = urlencode($to_Currency);
+				$to_Currency = urlencode($cur->expect_worth_name);
+				$to_Currency_id = urlencode($cur->expect_worth_id);
+				/* $url = "http://www.google.com/ig/calculator?hl=en&q=$amount$from_Currency=?$to_Currency";
+				$ch = curl_init();
+				$timeout = 0;
+				curl_setopt ($ch, CURLOPT_URL, $url);
+				curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+				curl_setopt($ch,  CURLOPT_USERAGENT , "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1)");
+				curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+				$rawdata = curl_exec($ch);
+				curl_close($ch);
+				
+				$data = explode('"', $rawdata);
+				$data = explode(' ', $data['3']); 
+				$var = $data['0']; */
+				
+				if($from_Currency!=$to_Currency) {
+					$url = "https://www.google.com/finance/converter?a=$amount&from=$from_Currency&to=$to_Currency";
+					$amount = urlencode($amount);
+					$from_Currency = urlencode($from_Currency);
+					$to_Currency = urlencode($to_Currency);
+					$get = file_get_contents("https://www.google.com/finance/converter?a=$amount&from=$from_Currency&to=$to_Currency");
+					$get = explode("<span class=bld>",$get);
+					$get = explode("</span>",$get[1]);  
+					$converted_amount = preg_replace("/[^0-9\.]/", null, $get[0]);
+					$conversion_value = round($converted_amount, 3);
+					updt_currency($from_Currency_id, $to_Currency_id, $conversion_value);
+				} else {
+					updt_currency($from_Currency_id, $to_Currency_id, 1);
+				}
+			}
 		}
-		updt_currency($to_Currency_id, $to_Currency_id, 1);
 	}
 }
 
@@ -83,8 +90,9 @@ function updt_currency($frm, $to_Currency_id, $conversion_value)
 	$CI->db->where('from', $frm);
 	$CI->db->where('to', $to_Currency_id);
 	$query1 = $CI->db->get($cfg['dbpref'].'currency_rate');
+
 	$res_num = $query1->num_rows();
-			
+
 	if($res_num>0) {
 		if(!empty($conversion_value)){
 			$CI->db->where('from', $frm);
