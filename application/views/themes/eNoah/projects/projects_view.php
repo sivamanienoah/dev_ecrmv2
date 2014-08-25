a461aec5f611ecec5891f05bb2586ffe6e862fb9
sriram <sriram@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2014-08-25 11:37:41 +0000

    legend changes

--- application/views/themes/eNoah/projects/projects_view.php
+++ application/views/themes/eNoah/projects/projects_view.php
@@ -132,14 +132,13 @@ if (get_default_currency()) {
 						<td><img src="assets/img/planned-hour.png"> - Planned Hour</td>
 						<td><img src="assets/img/billable-hour.png"> - Billable Hour</td>
 						<td><img src="assets/img/internal-hour.png"> - Internal Hour</td>
-						<td><img src="assets/img/non-billable.png"> - Non Billable Hour</td>
 					</tr>
 					<tr>
+						<td><img src="assets/img/non-billable.png"> - Non Billable Hour</td>
 						<td><img src="assets/img/total-hours.png"> - Total Utilized Hours</td>
 						<td><img src="assets/img/project_value.png"> - Project Value</td>
 						<td><img src="assets/img/utilization_project_value.png"> - Utilization Cost</td>
 						<td><img src="assets/img/profitloss.png"> - P&L </td>
-						<td><img src="assets/img/profitloss.png"> - P&L % </td>
 					</tr>
 				</table>
 			</div>
