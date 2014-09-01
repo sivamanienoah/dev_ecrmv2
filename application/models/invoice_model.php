01aeafdb7e83176df8cc49b0560867f31f8f8d96
sriram <sriram@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2014-09-01 11:27:42 +0000

    date issue for greater than or equal to

--- application/models/invoice_model.php
+++ application/models/invoice_model.php
@@ -45,15 +45,15 @@ class Invoice_model extends crm_model {
 			$this->db->where_in('l.practice', $filter['practice']);
 		}
 		if(!empty($filter['from_date']) && empty($filter['to_date'])) {
-			$this->db->where('expm.invoice_generate_notify_date >=', date('Y-m-d', strtotime($filter['from_date'])));
+			$this->db->where('DATE(expm.invoice_generate_notify_date) >=', date('Y-m-d', strtotime($filter['from_date'])));
 		} else if(!empty($filter['from_date']) && !empty($filter['to_date'])) {
-			$this->db->where('expm.invoice_generate_notify_date >=', date('Y-m-d', strtotime($filter['from_date'])));
-			$this->db->where('expm.invoice_generate_notify_date <=', date('Y-m-d', strtotime($filter['to_date'])));
+			$this->db->where('DATE(expm.invoice_generate_notify_date) >=', date('Y-m-d', strtotime($filter['from_date'])));
+			$this->db->where('DATE(expm.invoice_generate_notify_date) <=', date('Y-m-d', strtotime($filter['to_date'])));
 		} else {
 			$from = date('Y-m-01');
 			$end  = date('Y-m-t');
-			$this->db->where('expm.invoice_generate_notify_date >=', $from);
-			$this->db->where('expm.invoice_generate_notify_date <=', $end);
+			$this->db->where('DATE(expm.invoice_generate_notify_date) >=', $from);
+			$this->db->where('DATE(expm.invoice_generate_notify_date) <=', $end);
 		}
 		$query  = $this->db->get();
 		// echo $this->db->last_query();
