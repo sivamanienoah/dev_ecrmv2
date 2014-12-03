a6e546ad8a9b53cdf73c9ce070ba268715e1d4ef
sriram <sriram@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2014-12-03 10:26:48 +0000

    timesheet calculation - final date issue fixed.

--- application/models/project_model.php
+++ application/models/project_model.php
@@ -502,7 +502,7 @@ class Project_model extends crm_model
 		LEFT JOIN ".$timesheet_db->dbprefix('user_cost')." as ct ON ct.employee_id = u.emp_id AND ct.month=MONTH(t.start_time) AND ct.year=YEAR(t.start_time) 
 		LEFT JOIN ".$timesheet_db->dbprefix('project')." as p ON p.proj_id = t.proj_id
 		LEFT JOIN ".$timesheet_db->dbprefix('project_types')." as pt ON pt.project_type_id = p.project_type_id
-		WHERE ((t.start_time >= '".$start_date."') AND (t.end_time <= '".$end_date."')) AND u.status='ACTIVE' AND p.project_code = '".$pjt_code."'
+		WHERE ((DATE(t.start_time) >= '".$start_date."') AND (DATE(t.end_time) <= '".$end_date."')) AND u.status='ACTIVE' AND p.project_code = '".$pjt_code."'
 		GROUP BY cost, u.first_name, u.last_name, u.username, month_name, t.resoursetype
 		ORDER BY yr, month_name, Week, u.first_name, u.last_name, u.username, t.resoursetype, WEEKDAY(t.start_time)";
 		
