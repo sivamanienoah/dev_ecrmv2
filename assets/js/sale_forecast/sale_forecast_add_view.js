0e524a5f5c74957348d30916cb5fdc9875ec51b4
sriram <sriram@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2015-04-02 13:38:55 +0000

    Sale forecast module changes

--- assets/js/sale_forecast/sale_forecast_add_view.js
+++ assets/js/sale_forecast/sale_forecast_add_view.js
@@ -4,8 +4,6 @@
 */
 // csrf_token_name,csrf_hash_token,site_base_url & accesspageis global js variable
 
-$('#advance_search').hide();
-
 $('#lead-data').hide();
 $('#project-data').hide();
 $('#leaddetail').hide();
@@ -58,33 +56,6 @@ $(function() {
 				iMonth = jQuery.inArray(selDate.substring(0, selDate.length - 5), $(this).datepicker('option', 'monthNames'));
 				$(this).datepicker('option', 'defaultDate', new Date(iYear, iMonth, 1));
 				$(this).datepicker('setDate', new Date(iYear, iMonth, 1));
-			}
-			$('#ui-datepicker-div')[ $(input).is('[data-calendar="false"]') ? 'addClass' : 'removeClass' ]('hide-calendar');
-		}
-	});
-	
-	$( "#month_year_from_date, #month_year_to_date" ).datepicker({
-		changeMonth: true,
-		changeYear: true,
-		dateFormat: 'MM yy',            
-		onClose: function(dateText, inst) {
-			var month = $("#ui-datepicker-div .ui-datepicker-month :selected").val();
-			var year = $("#ui-datepicker-div .ui-datepicker-year :selected").val();         
-			$(this).datepicker('setDate', new Date(year, month, 1));
-		},
-		beforeShow : function(input, inst) {
-			if ((datestr = $(this).val()).length > 0) {
-				year = datestr.substring(datestr.length-4, datestr.length);
-				month = jQuery.inArray(datestr.substring(0, datestr.length-5), $(this).datepicker('option', 'monthNames'));
-				$(this).datepicker('option', 'defaultDate', new Date(year, month, 1));
-				$(this).datepicker('setDate', new Date(year, month, 1));    
-			}
-				var other  = this.id  == "month_year_from_date" ? "#month_year_to_date" : "#month_year_from_date";
-				var option = this.id == "month_year_from_date" ? "maxDate" : "minDate";        
-			if ((selectedDate = $(other).val()).length > 0) {
-				year = selectedDate.substring(selectedDate.length-4, selectedDate.length);
-				month = jQuery.inArray(selectedDate.substring(0, selectedDate.length-5), $(this).datepicker('option', 'monthNames'));
-				$(this).datepicker( "option", option, new Date(year, month, 1));
 			}
 			$('#ui-datepicker-div')[ $(input).is('[data-calendar="false"]') ? 'addClass' : 'removeClass' ]('hide-calendar');
 		}
@@ -209,37 +180,6 @@ function get_lead_detail(id, sf_id) {
 	
 }
 
-function advanced_filter() {
-	$('#advance_search').slideToggle('slow');
-}
-
-//For Advance Filters functionality.
-$("#advanceFiltersForecast").submit(function() {
-	$('#advance').hide();
-	$('#load').show();
-	var entity     = $("#entity").val();
-	var customer   = $("#customer").val();
-	var lead_names = $("#lead_names").val();
-	var month_year_from_date = $("#month_year_from_date").val();
-	var month_year_to_date   = $("#month_year_to_date").val();
-
-	$.ajax({
-		type: "POST",
-		url: site_base_url+"sales_forecast/index/",
-		// dataType: "json",
-		data: "filter=filter"+"&lead_names="+lead_names+"&customer="+customer+"&entity="+entity+'&month_year_from_date='+month_year_from_date+"&month_year_to_date="+month_year_to_date+"&"+csrf_token_name+'='+csrf_hash_token,
-		beforeSend:function(){
-			$('#results').empty();
-			$('#results').html('<div style="margin:20px;" align="center">Loading Content.<br><img alt="wait" src="'+site_base_url+'assets/images/ajax_loader.gif"><br>Thank you for your patience!</div>');
-		},
-		success: function(res) {
-			$('#advance').show();
-			$('#results').html(res);
-			$('#load').hide();
-		}
-	});
-	return false;  //stop the actual form post !important!
-});
 
 //Adding multiple milestone rows - Start here
 
--- assets/js/sale_forecast/sale_forecast_view.js
+++ assets/js/sale_forecast/sale_forecast_view.js
@@ -4,19 +4,40 @@
 */
 
 // csrf_token_name,csrf_hash_token,site_base_url & accesspageis global js variable
-	
-$('.data-tbl').dataTable({
-	"aaSorting": [[ 6, "desc" ]],
-	"iDisplayLength": 10,
-	"sPaginationType": "full_numbers",
-	"bInfo": true,
-	"bPaginate": true,
-	"bProcessing": true,
-	"bServerSide": false,
-	"bLengthChange": true,
-	"bSort": true,
-	"bFilter": true,
-	"bAutoWidth": false,	
+
+$('#advance_search').hide();
+
+function advanced_filter() {
+	$('#advance_search').slideToggle('slow');
+}
+
+//For Advance Filters functionality.
+$("#advanceFiltersForecast").submit(function() {
+	$('#advance').hide();
+	$('#load').show();
+	var entity      = $("#entity").val();
+	var customer    = $("#customer").val();
+	var lead_ids    = $("#lead_ids").val();
+	var project_ids = $("#project_ids").val();
+	var month_year_from_date = $("#month_year_from_date").val();
+	var month_year_to_date   = $("#month_year_to_date").val();
+
+	$.ajax({
+		type: "POST",
+		url: site_base_url+"sales_forecast/index/",
+		// dataType: "json",
+		data: "filter=filter"+"&lead_ids="+lead_ids+"&project_ids="+project_ids+"&customer="+customer+"&entity="+entity+'&month_year_from_date='+month_year_from_date+"&month_year_to_date="+month_year_to_date+"&"+csrf_token_name+'='+csrf_hash_token,
+		beforeSend:function(){
+			$('#results').empty();
+			$('#results').html('<div style="margin:20px;" align="center">Loading Content.<br><img alt="wait" src="'+site_base_url+'assets/images/ajax_loader.gif"><br>Thank you for your patience!</div>');
+		},
+		success: function(res) {
+			$('#advance').show();
+			$('#results').html(res);
+			$('#load').hide();
+		}
+	});
+	return false;  //stop the actual form post !important!
 });
 
 function checkStatus(id) {
@@ -34,4 +55,32 @@ function processDelete(id) {
 function cancelDel() {
     $.unblockUI();
 }
+
+$( "#month_year_from_date, #month_year_to_date" ).datepicker({
+	changeMonth: true,
+	changeYear: true,
+	dateFormat: 'MM yy',
+	showButtonPanel: true,	
+	onClose: function(dateText, inst) {
+		var month = $("#ui-datepicker-div .ui-datepicker-month :selected").val();
+		var year = $("#ui-datepicker-div .ui-datepicker-year :selected").val();         
+		$(this).datepicker('setDate', new Date(year, month, 1));
+	},
+	beforeShow : function(input, inst) {
+		if ((datestr = $(this).val()).length > 0) {
+			year = datestr.substring(datestr.length-4, datestr.length);
+			month = jQuery.inArray(datestr.substring(0, datestr.length-5), $(this).datepicker('option', 'monthNames'));
+			$(this).datepicker('option', 'defaultDate', new Date(year, month, 1));
+			$(this).datepicker('setDate', new Date(year, month, 1));    
+		}
+			var other  = this.id  == "month_year_from_date" ? "#month_year_to_date" : "#month_year_from_date";
+			var option = this.id == "month_year_from_date" ? "maxDate" : "minDate";        
+		if ((selectedDate = $(other).val()).length > 0) {
+			year = selectedDate.substring(selectedDate.length-4, selectedDate.length);
+			month = jQuery.inArray(selectedDate.substring(0, selectedDate.length-5), $(this).datepicker('option', 'monthNames'));
+			$(this).datepicker( "option", option, new Date(year, month, 1));
+		}
+		$('#ui-datepicker-div')[ $(input).is('[data-calendar="false"]') ? 'addClass' : 'removeClass' ]('hide-calendar');
+	}
+});
 //////////////////////////////////////////////////////////////////// end ///////////////////////////////////////////////////
