b664e0c3002a4a4318f1cd2bcfa0101b418c6377
thiyagarajan.m <thiyagarajan.m@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2015-04-22 09:01:31 +0000

    applied stake holders

--- application/models/project_model.php
+++ application/models/project_model.php
@@ -121,14 +121,21 @@ class Project_model extends crm_model
 			$rowsJobs = $this->db->get($this->cfg['dbpref'] . 'leads');
 			$data['jobids1'] = $rowsJobs->result_array();
 
-			//Fetching Stake Holders.
+			//Fetching Stake Holders from active projects.
 			$data['jobids2'] = array();
-			$this->db->select('lead_id');
-			$this->db->where("user_id",$varSessionId);
-			$this->db->where("lead_status", 4);
-			$this->db->where("pjt_status", 1);			
-			$rowsJobs = $this->db->get($this->cfg['dbpref'] . 'stake_holders');
-			if($rowsJobs->num_rows()>0)	$data['jobids2'] = $rowsJobs->result_array();			
+			$this->db->select('sh.lead_id');
+			$this->db->from($this->cfg['dbpref'] . 'stake_holders as sh');
+			$this->db->join($this->cfg['dbpref'] . 'leads as leads','sh.lead_id = leads.lead_id');
+			$this->db->where("sh.user_id",$varSessionId);						
+			$this->db->where("leads.lead_status", 4);
+			$this->db->where("leads.pjt_status", 1);					
+			$rowsJobs = $this->db->get();
+			
+			 
+			
+			if($rowsJobs->num_rows()>0)	{
+				$data['jobids2'] = $rowsJobs->result_array();				
+			}
 			
 			$data = array_merge_recursive($data['jobids'], $data['jobids1'],$data['jobids2']);
 
