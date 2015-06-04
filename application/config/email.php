e6a5e38aebce8aa5ea4660b47d7c8b036228943f
sriram <sriram@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2015-06-04 11:59:40 +0000

    smtp_port added

--- application/config/email.php
+++ application/config/email.php
@@ -7,6 +7,7 @@ $config['protocol']  = "mail";	// mail/sendmail/smtp
 // $config['smtp_host'] = "192.168.0.201";	// SMTP Server.  Example: mail.earthlink.net
 // $config['smtp_user'] = "webmaster@enoahisolution.com";	// SMTP Username
 // $config['smtp_pass'] = "eNoah123#";		// SMTP Password
+$config['smtp_port'] = "587";		// SMTP Password
 $config['mailtype']  = 'html';  // text/html  Defines email formatting
 $config['wordwrap']  = TRUE;  // TRUE/FALSE  Turns word-wrap on/off
 $config['charset']   = "utf-8";  // Default char set: iso-8859-1 or us-ascii
