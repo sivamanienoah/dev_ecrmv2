00b61b99acf1a818926994f46adadb81f03c5f0d
ramkumar.m <ramkumar.m@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2016-10-06 11:56:04 +0000

    Task Module-update with Category,priority and data listing in pagination

--- application/models/task_model.php
+++ application/models/task_model.php
@@ -21,7 +21,10 @@ class Task_model extends crm_model
 			if($task_search['task_end_notify']!="" )
 			{
 				$task_notify_status = get_notify_status(2);
-				$query_end_notify="AND `".$this->cfg['dbpref']."tasks`.end_date BETWEEN CURDATE() AND DATE(DATE_ADD(CURDATE(), INTERVAL ".$task_notify_status." DAY))";
+				$query_end_notify="AND `".$this->cfg['dbpref']."tasks`.end_date BETWEEN CURDATE() AND DATE(DATE_ADD(CURDATE(), INTERVAL ".$task_notify_status." DAY))
+									AND `".$this->cfg['dbpref']."tasks`.end_date='0000-00-00 00:00:00'";
+			
+				
 			}
 			else
 			{
