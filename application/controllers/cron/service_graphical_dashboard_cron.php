9c553c23cd569fae581dd6655d20e5012d2c1312
sriram <sriram@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2016-10-27 11:05:28 +0000

    test

--- application/controllers/cron/service_graphical_dashboard_cron.php
+++ application/controllers/cron/service_graphical_dashboard_cron.php
@@ -399,6 +399,7 @@ class Service_graphical_dashboard_cron extends crm_controller
 					$other_cos_arr = getOtherCostByProjectCodeByDateRangeByMonthWise($prec->pjt_id, $this->default_cur_id, $start_date, $end_date);
 					if(isset($contribution_trend_project_arr[$prec->pjt_id])) {
 						$contribution_project_arr = $this->combineContributionProjectArr($practice_arr[$prec->practice], $contribution_trend_project_arr[$prec->pjt_id]);
+						echo "<pre>"; print_r($contribution_project_arr); exit;
 					}
 					$contribution_trend_arr = $this->combineOthercostContributionCost($practice_arr[$prec->practice], $contribution_trend_arr, $contribution_project_arr, $other_cos_arr);
 				}
@@ -518,7 +519,6 @@ class Service_graphical_dashboard_cron extends crm_controller
 	
 	public function combineContributionProjectArr($practice_name, $project_mon_arr)
 	{
-		echo "asdf".$practice_name; print_r($project_mon_arr); exit;
 		$res_arr = array();
 		$res_arr = $project_mon_arr;
 		if(!empty($project_mon_arr) && count($project_mon_arr)>0) {
