49a6e8ec3a2d5e0f741d4fc98875506d4d315ed8
sriram <sriram@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2016-10-31 10:51:10 +0000

    test

--- application/controllers/cron/service_graphical_dashboard_cron.php
+++ application/controllers/cron/service_graphical_dashboard_cron.php
@@ -461,7 +461,7 @@ class Service_graphical_dashboard_cron extends crm_controller
 				/**other cost data*/
 				//for billable efforts
 				$bill_eff = 0;
-				echo "<pre>"; print_r($projects['billable_ytd'][$parr]);  . "<br>";
+				echo "<pre>  <br>"; print_r($projects['billable_ytd'][$parr]);
 				if(isset($projects['billable_ytd'][$parr]) && !empty($projects['billable_ytd'][$parr])) {
 					$bill_eff = (($projects['billable_ytd'][$parr]['Billable']['hour'])/$projects['billable_ytd'][$parr]['totalhour'])*100;		
 				}
