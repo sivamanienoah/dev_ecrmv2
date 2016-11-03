89951f9f7da0d9dd3d3e0cbc5e9c1f9446d0f224
sriram <sriram@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2016-11-03 04:27:44 +0000

    customize issue fixed

--- application/views/themes/eNoah/projects/projects_view_inprogress_monthly.php
+++ application/views/themes/eNoah/projects/projects_view_inprogress_monthly.php
@@ -109,7 +109,7 @@ if(!empty($db_fields) && count($db_fields)>0){
 				$monthly_content .= "<td>".$profitloss."</td>";
 				$monthly_content .= "<td><span class=".$prof_clr.">".$profitlossPercent." %</span></td>";
 			} else {
-				if(($td_cn == true) && in_array('CN', $db_fields)) { $td_cn = 'style="display: table-cell;"'; }
+				if(($td_chk == true) && in_array('CN', $db_fields)) { $td_cn = 'style="display: table-cell;"'; }
 				$milestone_content .= "<td ".$td_cn.">".$customer_name."</td>";
 				if(($td_chk == true) && in_array('CP', $db_fields)) { $td_cp = 'style="display: table-cell;"'; }
 				$monthly_content .= "<td ".$td_cp.">".$complete_stat."</td>";
