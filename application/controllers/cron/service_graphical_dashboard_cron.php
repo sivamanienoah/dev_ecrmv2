339df23ab03985fde13bc5f3edcdd13db446faf4
sriram <sriram@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2016-11-05 10:16:55 +0000

    test

--- application/controllers/cron/service_graphical_dashboard_cron.php
+++ application/controllers/cron/service_graphical_dashboard_cron.php
@@ -516,7 +516,7 @@ class Service_graphical_dashboard_cron extends crm_controller
 				$ins_array = array();
 				$ins_result = 1;
 			}
-			
+			exit;
 			$tot['ytd_billable'] 		 		  = round(($tot_bill_eff/$tot_tot_bill_eff)*100);
 			$tot['ytd_billable_utilization_cost'] = round(($tot_temp_billable_ytd_uc/$tot_temp_ytd_uc)*100);
 			$tot['tot_contri'] 			  		  = round((($overall_revenue-$overall_contrib)/$overall_revenue)*100);
