ae8d21a2c35eefeab31beb2a083fc6b4fe07b804
dhanapal.p <dhanapal.p@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2017-03-07 05:27:48 +0000

    Import timesheet user to crm

--- application/controllers/cron/create_timesheet_user.php
+++ application/controllers/cron/create_timesheet_user.php
@@ -52,10 +52,12 @@ class Create_timesheet_user extends crm_controller
 		
 		$timesheet_db = $this->load->database('timesheet', TRUE);
 		
-		$time_sheet_users_sql = "SELECT * FROM ".$timesheet_db->dbprefix('user')." WHERE `authentication`='ldb' AND `status` = 'ACTIVE'";
-	
-		$time_sheet_query  = $timesheet_db->query($time_sheet_users_sql);
-		$timesheet_users = $time_sheet_query->result_array();
+		$timesheet_db->select('*');
+		$timesheet_db->from($timesheet_db->dbprefix('user'));	
+		$timesheet_db->where('authentication','ldb');		
+		$timesheet_db->where('status','ACTIVE');	
+		$time_sheet_query = $timesheet_db->get();
+		$timesheet_users  = $time_sheet_query->result_array();
 		
 		foreach($timesheet_users as $eusers){
 			//1.check whether the username exists or not in CRM DB.
@@ -67,7 +69,7 @@ class Create_timesheet_user extends crm_controller
 			
 			if($query->num_rows() == 0) { 
 			//check email
-				if(!in_array($eusers['email'],$crm_email)) {
+				if(!in_array($eusers['email'], $crm_email)) {
 					//insert into crm db
 					$data = array(
 					   'role_id' => 8,
@@ -76,18 +78,16 @@ class Create_timesheet_user extends crm_controller
 					   'username' => $eusers['username'],
 					   'emp_id' => $eusers['uid'],
 					   'password' => $eusers['password'],
-					   //'password' => sha1('admin123'),
 					   'email' => $eusers['email_address'],
 					   'phone' => '',
 					   'mobile' => '',
 					   'level' => 1,
-					   'auth_type' => 1,
+					   'auth_type' => 0,
 					   'signature' => '',
 					   'inactive' => 0
 					);
 					if($eusers['email_address']!=''){
 						if($this->db->insert($this->cfg['dbpref'].'users', $data)) {
-							// $user_success[] = $eusers['empid'].' - '.$eusers['username']." - New user created.";
 							$user_success[] = $eusers['uid'].' => '.$eusers['username'];
 							$crm_email[] = $eusers['email_address'];
 						}
@@ -108,10 +108,10 @@ class Create_timesheet_user extends crm_controller
 		if(!empty($user_success)){
 			$from		  	 = 'webmaster@enoahprojects.com';
 			$arrayEmails   	 = $this->config->item('crm');
-			$to				 = implode(',',$arrayEmails['crm_admin']);
+			$to				 = implode(',', $arrayEmails['crm_admin']);
 			$print_fancydate = date('l, jS F y h:iA');
 			
-			$subject		 = 'New User List from eConnect';
+			$subject		 = 'New User List from Timesheet';
 			
 			$user_list  = '<table border=1>';
 			$user_list .= '<tr><th style="font-family: Arial,Helvetica,sans-serif; font-size:12px">Emp ID</th><th style="font-family: Arial,Helvetica,sans-serif; font-size:12px">UserName</th></tr>';
@@ -125,12 +125,9 @@ class Create_timesheet_user extends crm_controller
 			
 			$param['email_data'] = array('print_fancydate'=>$print_fancydate,'user_list'=>$user_list);
 
-			//$param['to_mail'] 		  = $to;
-			$param['to_mail'] 		  = "pdhanapal@enoahisolution.com";
+			$param['to_mail'] 		  = $to;
 			//$param['bcc_mail'] 		  = 'ssriram@enoahisolution.com';
-			$param['from_email']	  = 'webmaster@enoahprojects.com';
-			$param['from_email_name'] = 'Webmaster';
-			$param['template_name']	  = 'New User List from eConnect';
+			$param['template_name']	  = 'New User List from Timesheet';
 			$param['subject'] 		  = $subject;
 
 			$this->load->model('email_template_model');
@@ -142,7 +139,7 @@ class Create_timesheet_user extends crm_controller
 			$to				 = implode(',',$arrayEmails['crm_admin']);
 			$print_fancydate = date('l, jS F y h:iA');
 			
-			$subject		 = 'Failed User List from eConnect';
+			$subject		 = 'Failed User List from Timesheet';
 			$user_lists  = '<table border=1>';
 			$user_lists .= '<tr><th style="font-family: Arial,Helvetica,sans-serif; font-size:12px">Emp ID</th><th style="font-family: Arial,Helvetica,sans-serif; font-size:12px">UserName</th><th style="font-family: Arial,Helvetica,sans-serif; font-size:12px">Reason</th></tr>';
 			foreach($user_failed as $faileduser) {
@@ -155,12 +152,9 @@ class Create_timesheet_user extends crm_controller
 			
 			$param['email_data'] = array('print_fancydate'=>$print_fancydate,'user_list'=>$user_lists);
 
-			//$param['to_mail'] 		  = $to;
-			$param['to_mail'] 		  = "pdhanapal@enoahisolution.com";
+			$param['to_mail'] 		  = $to;
 			//$param['bcc_mail'] 		  = 'ssriram@enoahisolution.com';
-			$param['from_email']	  = 'webmaster@enoahprojects.com';
-			$param['from_email_name'] = 'Webmaster';
-			$param['template_name']	  = 'Failed User List from eConnect';
+			$param['template_name']	  = 'Failed User List from Timesheet';
 			$param['subject'] 		  = $subject;
 
 			$this->load->model('email_template_model');
