6c3d289b7f98e7e88d7fc52c08e0c54ed3676753
sriram <sriram@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2017-05-03 10:23:06 +0000

    test

--- application/controllers/projects/dashboard.php
+++ application/controllers/projects/dashboard.php
@@ -411,10 +411,10 @@ class Dashboard extends crm_controller
 		
 		$query = $this->db->get(); */
 		
-		
+		echo "asdfa"; die;
 		$this->db->select('t.dept_id, t.dept_name, t.practice_id, t.practice_name, t.skill_id, t.skill_name, t.resoursetype, t.username, t.duration_hours, t.resource_duration_cost, t.cost_per_hour, t.project_code, t.empname, t.direct_cost_per_hour, t.resource_duration_direct_cost,t.entry_month as month_name, t.entry_year as yr, t.entity_id, t.entity_name');
 		$this->db->from($this->cfg['dbpref']. 'timesheet_month_data as t');
-		$this->db->join($this->cfg['dbpref'].'leads as l', 'l.pjt_id = t.project_code', 'left');
+		$this->db->join($this->cfg['dbpref']. 'leads as l', 'l.pjt_id = t.project_code', 'LEFT');
 		$this->db->where("t.resoursetype !=", 0);
 		/* if(!empty($start_date) && !empty($end_date)) {
 			$this->db->where("(t.start_time >='".date('Y-m-d', strtotime($start_date))."' )", NULL, FALSE);
