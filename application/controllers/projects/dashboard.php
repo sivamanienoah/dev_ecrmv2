0c15d3a5ea8959601f49e26efd72d9c68a20c1be
sriram <sriram@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2017-05-03 13:18:31 +0000

    test

--- application/controllers/projects/dashboard.php
+++ application/controllers/projects/dashboard.php
@@ -973,7 +973,6 @@ class Dashboard extends crm_controller
 		$data['filter_sort_val'] = $this->input->post("filter_sort_val");
 		else
 		$data['filter_sort_val'] = 'hour';
-	echo $this->input->post("filter_group_by"); die;
 	
 		switch($this->input->post("filter_group_by")){
 			case 0:
