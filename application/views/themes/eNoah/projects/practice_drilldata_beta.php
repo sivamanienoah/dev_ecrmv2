8678760d3f09efdbd9bd0256278440f6e0fbffd2
sriram <sriram@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2017-05-03 14:50:30 +0000

    test

--- application/views/themes/eNoah/projects/practice_drilldata_beta.php
+++ application/views/themes/eNoah/projects/practice_drilldata_beta.php
@@ -128,7 +128,7 @@ if(!empty($resdata)) {
 										foreach($resrc_data as $key2=>$value2) {
 											$year = $key2;
 											if(count($value2)>0 && !empty($value2)){
-												foreach($value2 as $key3=>$value3) {
+												foreach($value2 as $key3=>$value3) { echo "<pre>"; print_r($value3); die;
 													$individual_billable_hrs = 0;
 													$ts_month		 	  	 = $key3;
 													if(count($value3)>0 && !empty($value3)) {
@@ -147,9 +147,9 @@ if(!empty($resdata)) {
 																}
 																
 																if(isset($tbl_data[$dept_key][$prac_key][$skill_key][$emp_name][$pjt_code]['hour'])) {
-																	$tbl_data[$dept_key][$prac_key][$skill_key][$emp_name][$pjt_code]['hour'] += $rec->duration_hours;
+																	$tbl_data[$dept_key][$prac_key][$skill_key][$emp_name][$pjt_code]['hour'] += $duration_hours;
 																} else {
-																	$tbl_data[$dept_key][$prac_key][$skill_key][$emp_name][$pjt_code]['hour'] = $rec->duration_hours;
+																	$tbl_data[$dept_key][$prac_key][$skill_key][$emp_name][$pjt_code]['hour']  = $duration_hours;
 																}
 																
 															}
