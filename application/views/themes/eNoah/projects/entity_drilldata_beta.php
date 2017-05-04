6eef00450d4c5dfc636dd6e8d5221184da6668aa
sriram <sriram@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2017-05-04 07:07:16 +0000

    test

--- application/views/themes/eNoah/projects/entity_drilldata_beta.php
+++ application/views/themes/eNoah/projects/entity_drilldata_beta.php
@@ -114,7 +114,7 @@ if(!empty($resdata)) {
 	// echo "<pre>"; print_r($timesheet_data);
 	
 	if(!empty($timesheet_data) && count($timesheet_data)>0) {
-		foreach($timesheet_data as $entity_key=>$entity_arr) { echo "entity_key " .$entity_key; die;
+		foreach($timesheet_data as $entity_key=>$entity_arr) { echo "<pre>"; print_R($entity_arr); die;
 			if(!empty($entity_arr) && count($entity_arr)>0) {
 				foreach($entity_arr as $dept_key=>$prac_arr) {
 					if(!empty($prac_arr) && count($prac_arr)>0) {
