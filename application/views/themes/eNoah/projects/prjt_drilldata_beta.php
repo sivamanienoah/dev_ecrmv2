4bdde009079a24e413a133f737c0c3faeaaeb14d
sriram <sriram@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2017-05-04 04:34:11 +0000

    test

--- application/views/themes/eNoah/projects/prjt_drilldata_beta.php
+++ application/views/themes/eNoah/projects/prjt_drilldata_beta.php
@@ -173,8 +173,13 @@ if(!empty($resdata)) {
 								foreach($resrc_data as $resrc_name=>$recval_data) {
 									if(count($recval_data)>0 && !empty($recval_data)) {
 										foreach($recval_data as $key2=>$value2) {
-											echo $year = $key2;
-											// echo "<pre>"; print_r($value2);
+											$year = $key2;
+											if(count($value2)>0 && !empty($value2)) {
+												foreach($value2 as $key3=>$value3) {
+													$individual_billable_hrs = 0;
+													echo $ts_month 				 = $key3;
+												}
+											}
 										}
 									}
 								}
