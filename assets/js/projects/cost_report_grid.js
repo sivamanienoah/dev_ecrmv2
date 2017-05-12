f1beb5e0d20da5b749b9e497b5f8dcd5ff230659
sriram <sriram@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2017-05-12 09:13:53 +0000

    test

--- assets/js/projects/cost_report_grid.js
+++ assets/js/projects/cost_report_grid.js
@@ -1,5 +1,14 @@
 $(function() {
 	
+	//export
+	$("#btnExport").click(function () {
+		$("#it_cost_grid").btechco_excelexport({
+			containerid: "it_cost_grid"
+		   , datatype: $datatype.Table
+		   , filename: 'cost_report'
+		});
+	});
+	
 	$("#reset_drilldown").click(function(){
 		$('#filter_group_by').prop('selectedIndex',0);
 		$('#filter_sort_by').prop('selectedIndex','desc');
