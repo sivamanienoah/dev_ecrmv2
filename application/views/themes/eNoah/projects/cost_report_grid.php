f0d15600f21b52cba25c2536ec867d1db51b478f
sriram <sriram@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2017-05-12 11:58:20 +0000

    test

--- application/views/themes/eNoah/projects/cost_report_grid.php
+++ application/views/themes/eNoah/projects/cost_report_grid.php
@@ -242,7 +242,7 @@ if(!empty($tbl_data)) {
 		}
 	}
 	echo "<tr>
-		<td colspan = '8'>Total:</td>
+		<td colspan='8' class='rt-ali'>Total:</td>
 		<td width='5%' align='right' class='rt-ali'>".round($tot_hour, 1)."</td>
 		<td width='5%' align='right' class='rt-ali'>".round($tot_cost, 2)."</td>
 		<td width='5%' align='right' class='rt-ali'>".round($tot_directcost, 2)."</td>
