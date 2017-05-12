36dacf380841d2179c67456f55aa3af419801dc1
sriram <sriram@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2017-05-12 09:09:01 +0000

    test

---
+++ assets/js/projects/cost_report_grid.js
@@ -0,0 +1,9 @@
+$(function() {
+	
+	$("#reset_drilldown").click(function(){
+		$('#filter_group_by').prop('selectedIndex',0);
+		$('#filter_sort_by').prop('selectedIndex','desc');
+		$('#filter_sort_val').prop('selectedIndex','hour');
+	});
+	
+});
