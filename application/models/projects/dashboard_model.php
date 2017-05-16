3e046ccb630e2e4e5990d16df9c670dd3c08f42c
sriram <sriram@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2017-05-16 05:40:55 +0000

    test

--- application/models/projects/dashboard_model.php
+++ application/models/projects/dashboard_model.php
@@ -90,6 +90,7 @@ class Dashboard_model extends crm_model
 			foreach($data as $row) {
 				$other_cost_array[$row['pjt_id']][date('Y', strtotime($row['cost_incurred_date']))][date('F', strtotime($row['cost_incurred_date']))] = $row;
 			}
+			echo "<pre>"; print_r($bk_rates); echo "<br>****<br>";
 			echo "<pre>"; print_r($other_cost_array); die;
 		}
 		return $other_cost_array;
