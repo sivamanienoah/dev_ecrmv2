d41c6980c2a61c950c014eea1264cbf5b17d2bcb
sriram <sriram@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2017-05-17 05:29:11 +0000

    test

--- application/views/themes/eNoah/projects/cost_report_grid.php
+++ application/views/themes/eNoah/projects/cost_report_grid.php
@@ -237,7 +237,7 @@ if(!empty($needAddOtherCostArr)) {
 <?php
 $perc_tot_hr = $perc_tot_cost = $calc_tot_hour = $calc_tot_cost = 0;
 
-	echo "<table id='it_cost_grid' class='data-tbl dashboard-heads dataTable'>
+	echo "<table id='it_cost_grid' class='data-tbl dashboard-heads dataTable it_cost_grid'>
 			<thead>
 			<tr>
 			<th class='prac-dt' width='10%'>ENTITY</th>
@@ -307,7 +307,7 @@ $perc_tot_hr = $perc_tot_cost = $calc_tot_hour = $calc_tot_cost = 0;
 		}
 	}
 	echo "</tbody></table>";
-	echo "<table class='it_cost_sub_grid'><tr>
+	echo "<table class='it_cost_sub_grid it_cost_grid'><tr>
 			<td width='75%' align='right' class=''><span><b>Total:</b></span></td>
 			<td width='5%' align='right' class='rt-ali'><span>".round($tot_hour, 1)."</span></td>
 			<td width='5%' align='right' class='rt-ali'><span>".round($tot_cost, 2)."</span></td>
