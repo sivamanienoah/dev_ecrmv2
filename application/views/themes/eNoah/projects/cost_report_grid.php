83f395c7dc0d8fd71e2f7ac426fb1fb482b4b0ac
sriram <sriram@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2017-05-18 06:41:24 +0000

    test

--- application/views/themes/eNoah/projects/cost_report_grid.php
+++ application/views/themes/eNoah/projects/cost_report_grid.php
@@ -219,7 +219,7 @@ if(!empty($needAddOtherCostArr)) {
 } */
 //other cost
 if(!empty($other_cost_arr)) {
-	foreach($other_cost_arr as $oc_year=>$oc_yearArr) {
+	foreach($other_cost_arr as $oc_pjt_code=>$oc_yearArr) {
 		if(!empty($oc_yearArr) && count($oc_yearArr)>0) {
 			foreach($oc_yearArr as $ocYrKey=>$ocYrArr) {
 				if(!empty($ocYrArr) && count($ocYrArr)>O) {
@@ -229,7 +229,7 @@ if(!empty($other_cost_arr)) {
 						$oc_prac_key 	= $ocArrRow['oc_practice'];
 						$oc_mon_yr 		= substr($ocMonthKey,0,3).' '.$ocYrKey;
 						$oc_other_cost_resrc_type = 'Other Cost';
-						$tbl_data[$oc_entity_key][$oc_dept_key][$oc_prac_key]['oc_skill'][$oc_other_cost_resrc_type][$oc_mon_yr][$row][$ocArrRow['oc_descrptn']]['cost'] = $ocArrRow['oc_val'];
+						$tbl_data[$oc_entity_key][$oc_dept_key][$oc_prac_key]['oc_skill'][$oc_other_cost_resrc_type][$oc_mon_yr][$oc_pjt_code][$ocArrRow['oc_descrptn']]['cost'] = $ocArrRow['oc_val'];
 					}
 				}
 			}
