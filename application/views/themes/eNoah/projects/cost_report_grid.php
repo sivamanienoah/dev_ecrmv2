f5c8c1d36cd717d27e00f3108ac2246d75e3e7fa
sriram <sriram@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2017-05-19 04:40:40 +0000

    test

--- application/views/themes/eNoah/projects/cost_report_grid.php
+++ application/views/themes/eNoah/projects/cost_report_grid.php
@@ -2,45 +2,10 @@
 .prac-dt{ text-align:center !important; }
 .toggle { display: inline-block; }
 .tr_othercost { background-color:#f2dede !important; }
-#it_cost_grid{
-	border-bottom:0px;
-}
-
+#it_cost_grid{ border-bottom:0px; }
 </style>
 <div class="clear"></div>
 <?php
-function array_sort($array, $on, $order='SORT_ASC')
-{
-    $new_array = array();
-    $sortable_array = array();
-
-    if (count($array) > 0) {
-        foreach ($array as $k => $v) {
-            if (is_array($v)) {
-                foreach ($v as $k2 => $v2) {
-                    if ($k2 == $on) {
-                        $sortable_array[$k] = $v2;
-                    }
-                }
-            } else {
-                $sortable_array[$k] = $v;
-            }
-        }
-        switch ($order) {
-            case 'SORT_ASC':
-                asort($sortable_array);
-                break;
-            case 'SORT_DESC':
-                arsort($sortable_array);
-                break;
-        }
-
-        foreach ($sortable_array as $k => $v) {
-            $new_array[$k] = $array[$k];
-        }
-    }
-    return $new_array;
-}
 $tbl_data = array();
 $sub_tot  = array();
 $sub_tot_hr    = array();
