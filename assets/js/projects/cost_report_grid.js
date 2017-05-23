b7c110f3c20bb581e5156d0f011937e62ffd1af0
sriram <sriram@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2017-05-23 06:48:13 +0000

    test

--- assets/js/projects/cost_report_grid.js
+++ assets/js/projects/cost_report_grid.js
@@ -52,9 +52,15 @@ if(filter_area_status==1) {
 	$('#advance_search').show();
 }
 
-$('#cost_rpt_search').on('keyup', function() {	
+$('#cost_rpt_search').on('keyup', function() {
 	var value = $(this).val();
 	if (value.length >= 3) {
+		
+		$.blockUI({
+			message:'<h4>Processing</h4><img src="assets/img/ajax-loader.gif" />',
+			css: {background:'#666', border: '2px solid #999', padding:'4px', height:'35px', color:'#333'}
+		});
+		
 		var patt = new RegExp(value, "i");
 
 		$('#it_cost_grid').find('tr').each(function() {
@@ -75,5 +81,6 @@ $('#cost_rpt_search').on('keyup', function() {
 			}
 		});
 		$("#it_cost_grid").tablesorter({widthFixed: false, widgets: ['zebra']});
+		$.unblockUI();
 	}	
 });
