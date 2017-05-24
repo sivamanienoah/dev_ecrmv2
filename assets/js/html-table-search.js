be2d7d091034f67d3b0f9afdf5c6231f666c4d7b
sriram <sriram@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2017-05-24 14:52:39 +0000

    cost report search changes

--- assets/js/html-table-search.js
+++ assets/js/html-table-search.js
@@ -12,7 +12,7 @@
 			searchText = (options.searchText)?options.searchText:'Search: ',
 			searchPlaceHolder = (options.searchPlaceHolder)?options.searchPlaceHolder:'',
 			divObj = $('<div id="srch_hide" style="float:right;">'+searchText+'</div><br /><br />'),
-			inputObj = $('<input type="text" class="search_input" placeholder="'+searchPlaceHolder+'" />'),
+			inputObj = $('<input type="text" class="textfield search_input" placeholder="'+searchPlaceHolder+'" />'),
 			caseSensitive = (options.caseSensitive===true)?true:false,
 			searchFieldVal = '',
 			pattern = '';
