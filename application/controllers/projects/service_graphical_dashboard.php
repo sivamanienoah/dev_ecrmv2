0d22f7fe04dbbe946131bee2c6c78edd2e450c22
sriram <sriram@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2017-06-27 13:54:52 +0000

    test

--- application/controllers/projects/service_graphical_dashboard.php
+++ application/controllers/projects/service_graphical_dashboard.php
@@ -97,7 +97,7 @@ class Service_graphical_dashboard extends crm_controller
 		
 		//get utilization cost values from service graphical dashboard table
 		$data['uc_graph_val'] = $this->service_graphical_dashboard_model->getUcRecords($uc_filter_by = 'cost', $data['fiscal_year_status']);
-		echo "<pre>"; print_r($data['uc_graph_val']); exit;
+		// echo "<pre>"; print_r($data['uc_graph_val']); exit;
 		
 		//get current fiscal ytd invoice records
 		$invoice_data = $this->service_graphical_dashboard_model->getInvoiceRecords($start_date, $end_date);
