d73b4c85158e346c1b8ab081b8a9908732d73ed5
sriram <sriram@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2017-07-28 12:56:41 +0000

    test email

--- application/config/email.php
+++ application/config/email.php
@@ -13,14 +13,14 @@
 // $config['wordwrap']  = TRUE;  // TRUE/FALSE  Turns word-wrap on/off
 // $config['charset']   = "utf-8";  // Default char set: iso-8859-1 or us-ascii 
 
-/* $config['protocol']  = "smtp";	// mail/sendmail/smtp
-$config['smtp_host'] = "192.168.0.201";	// SMTP Server.  Example: mail.earthlink.net
+$config['protocol']  = "smtp";	// mail/sendmail/smtp
+$config['smtp_host'] = "mail.enoahisolution.com";	// SMTP Server.  Example: mail.earthlink.net
 $config['smtp_user'] = "ssriram@enoahisolution.com";	// SMTP Username
-$config['smtp_pass'] = "dfdf$";		// SMTP Password
+$config['smtp_pass'] = "Raamsri1$";		// SMTP Password
 $config['smtp_port'] = "587";		// SMTP Password
 $config['mailtype']  = 'html';  // text/html  Defines email formatting
 $config['wordwrap']  = TRUE;  // TRUE/FALSE  Turns word-wrap on/off
-$config['charset']   = "utf-8";  // Default char set: iso-8859-1 or us-ascii  */
+$config['charset']   = "utf-8";  // Default char set: iso-8859-1 or us-ascii 
 
 
 ?>
