323f02afc1a920011488bba7162198b1a4504e79
sriram <sriram@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2017-08-10 05:10:00 +0000

    service dashboard enhancements changes

--- application/controllers/projects/it_service_dashboard.php
+++ application/controllers/projects/it_service_dashboard.php
@@ -353,7 +353,7 @@ class It_service_dashboard extends crm_controller
 					$timesheet_data[$rec->username][$rec->yr][$rec->month_name][$rec->project_code]['rateperhr']        = $rateCostPerHr;
 				}
 				// echo "get timesheet hours " . date('d-m-Y H:i:s') . "<br>";
-				if(count($timesheet_data)>0 && !empty($timesheet_data)) {
+				/* if(count($timesheet_data)>0 && !empty($timesheet_data)) {
 					foreach($timesheet_data as $dept_arr=>$resource_type_arr) {
 						if(!empty($resource_type_arr) && count($resource_type_arr)>0) {
 							foreach($resource_type_arr as $key1=>$value1) {
@@ -396,6 +396,47 @@ class It_service_dashboard extends crm_controller
 							}
 						}
 					}	
+				} */
+				
+				if(count($timesheet_data)>0 && !empty($timesheet_data)) {
+					foreach($timesheet_data as $key1=>$value1) {
+						$resource_name = $key1;
+						$max_hours = $value1['max_hours'];
+						$dept_name = $value1['dept_name'];
+						$resource_cost[$resource_name]['dept_name'] = $dept_name;
+						if(count($value1)>0 && !empty($value1)){
+							foreach($value1 as $key2=>$value2) {
+								$year = $key2;
+								if(count($value2)>0 && !empty($value2)){
+									foreach($value2 as $key3=>$value3) {
+										$individual_billable_hrs = 0;
+										$ts_month		 	  	 = $key3;
+										if(count($value3)>0 && !empty($value3)){
+											foreach($value3 as $key4=>$value4) {
+												if($key4 != 'total_hours'){ 
+													$individual_billable_hrs = $value3['total_hours'];
+													$duration_hours			 = $value4['duration_hours'];
+													$rate				 	 = $value4['rateperhr'];
+													$direct_rateperhr	 	 = $value4['direct_rateperhr'];
+													$rate1 					 = $rate;
+													$direct_rateperhr1 		 = $direct_rateperhr;
+													if($individual_billable_hrs>$max_hours) {
+														$percentage 		= ($max_hours/$individual_billable_hrs);
+														$rate1 				= number_format(($percentage*$direct_rateperhr),2);
+														$direct_rateperhr1  = number_format(($percentage*$direct_rateperhr),2);
+													}
+													$resource_cost[$resource_name][$year][$ts_month][$key4]['duration_hours'] += $duration_hours;
+													$resource_cost[$resource_name][$year][$ts_month][$key4]['total_cost'] 	  += ($duration_hours*$direct_rateperhr1);
+													$resource_cost[$resource_name][$year][$ts_month][$key4]['practice_id'] 	   = ($duration_hours*$rate1);
+													$resource_cost[$resource_name][$year][$ts_month][$key4]['total_dc_cost']  += ($duration_hours*$direct_rateperhr1);
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}	 
 				}
 			}
 			
