23ed2695af51673e89349fbc641e6cc93fc10a82
sriram <sriram@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2017-12-28 05:39:33 +0000

    test

--- application/helpers/custom_helper.php
+++ application/helpers/custom_helper.php
@@ -590,9 +590,7 @@ if ( ! function_exists('getOtherCostByLeadIdByDateRange'))
 			}
 			$CI->db->order_by('id', 'ASC');
 			$query  = $CI->db->get();
-			if('2017-04-01 00:00:00'==$end_date) {
-				echo $CI->db->last_query(); exit;
-			}
+			echo $CI->db->last_query().'<br>';
 			$result = $query->result_array();
 
 			if(count($result)>0 && !empty($result)) {
