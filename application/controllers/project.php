585a61cf96083edb18f6d58c66ace9219ee4fcd7
myvizhi.v <myvizhi.v@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2018-04-09 10:05:33 +0000

    Update actual date field with planned date in metrics tab if it is empty

--- application/controllers/project.php
+++ application/controllers/project.php
@@ -659,7 +659,6 @@ class Project extends crm_controller {
 			/* End leads estimated hours update */
 			
 			/* Update actual date field with planned date in metrics tab if it is empty */
-			// echo'<pre>';print_r($data['quote_data']);exit;
 			if(empty($data['quote_data']['actual_date_start'])){
 				$actual_date = $data['quote_data']['date_start'];
 				$wh_condn = array('lead_id' => $data['quote_data']['lead_id']);
@@ -669,6 +668,19 @@ class Project extends crm_controller {
 					$get_lead_res = $this->project_model->get_lead_det($data['quote_data']['lead_id']);
 					// echo'<pre>';print_r($get_lead_res);exit;
 					$data['quote_data']['actual_date_start'] = $get_lead_res['actual_date_start'];
+				}				
+				
+			}
+			
+			if(empty($data['quote_data']['actual_date_due'])){
+				$end_date = $data['quote_data']['date_due'];
+				$wh_condn = array('lead_id' => $data['quote_data']['lead_id']);
+				$updt = array('actual_date_due'=> $end_date);
+				$updt_data = $this->project_model->update_row('leads', $updt, $wh_condn);
+				if($updt_data){
+					$get_lead_res = $this->project_model->get_lead_det($data['quote_data']['lead_id']);
+					// echo'<pre>';print_r($get_lead_res);exit;
+					$data['quote_data']['actual_date_due'] = $get_lead_res['actual_date_due'];
 				}				
 				
 			}
