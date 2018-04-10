25f789ca06742bd83ba8c81ea3b194b8f56fb066
sriram.s <sriram.s@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2018-04-10 09:08:43 +0000

    project utilization cost report

--- application/views/themes/eNoah/projects/project-utiliz-rpt/utilization-cost-drill-data.php
+++ application/views/themes/eNoah/projects/project-utiliz-rpt/utilization-cost-drill-data.php
@@ -417,11 +417,9 @@ if(!empty($sub_tot)) {
 		}
 		
 	echo "<tfoot><tr data-depth='0'>
-		<td width='15%' align='right' class='rt-ali'><b>TOTAL:</b></td>
+		<td width='15%' colspan='2' align='right' class='rt-ali'><b>TOTAL:</b></td>
 		<td width='5%' align='right' class='rt-ali'><b>".round($calc_tot_hour, 1)."</b></td>
 		<td width='5%' align='right' class='rt-ali'><b>".round($invoices_data['total_amt'])."</b></td>
-		<td width='5%' align='right' class='rt-ali'><b>".round($calc_tot_directcost, 0)."</b></td>
-		<td width='5%' align='right' class='rt-ali'><b>".round($calc_tot_othercost, 0)."</b></td>
 		<td width='5%' align='right' class='rt-ali'><b>".round($calc_tot_cost, 0)."</b></td>
 		<td width='5%' align='right' class='rt-ali'><b></b></td>
 		<td width='5%' align='right' class='rt-ali'><b></b></td>
