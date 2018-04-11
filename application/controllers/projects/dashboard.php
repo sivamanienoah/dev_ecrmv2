770527545e79eb93881a67d3a6f8b7aefdd3ad29
sriram.s <sriram.s@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2018-04-11 14:31:16 +0000

    other cost excel report

--- application/controllers/projects/dashboard.php
+++ application/controllers/projects/dashboard.php
@@ -3792,7 +3792,7 @@ class Dashboard extends crm_controller
 			$practice_ids = array();
 		}
 		$other_cost_arr   = $this->dashboard_model->getOtherCosts($start_date, $end_date, $entity_ids, $practice_ids);
-		echo '<pre>123'.$this->db->last_query();; print_r($other_cost_arr); die;
+		// echo '<pre>123'.$this->db->last_query();; print_r($other_cost_arr); die;
 
 		//for practices		
 		$data['practice_ids'] 	  = $this->get_default_practices($start_date, $end_date);
