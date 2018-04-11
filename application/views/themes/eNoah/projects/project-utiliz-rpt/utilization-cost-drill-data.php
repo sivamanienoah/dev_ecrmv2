f71a9a36b2be33d9d547e6fadc49181fcb515af8
sriram.s <sriram.s@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2018-04-11 08:27:35 +0000

    project utilization cost report

--- application/views/themes/eNoah/projects/project-utiliz-rpt/utilization-cost-drill-data.php
+++ application/views/themes/eNoah/projects/project-utiliz-rpt/utilization-cost-drill-data.php
@@ -233,7 +233,7 @@ if(count($resource_cost)>0 && !empty($resource_cost)){
 	}
 }
 
-echo '<pre>'; print_r($sub_tot); echo '</pre>';
+// echo '<pre>'; print_r($sub_tot); echo '</pre>';
 
 //**Get the other cost value projects only**//
 $resource_cost_not_value_project = array_diff($othercost_projects[$practices_name], $timesheet_projects);
