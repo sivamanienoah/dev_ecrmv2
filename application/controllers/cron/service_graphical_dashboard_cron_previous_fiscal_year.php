e3f9843ff6f3673c644199fe85186ae16de9471b
sriram.s <sriram.s@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2018-04-14 06:36:17 +0000

    test

--- application/controllers/cron/service_graphical_dashboard_cron_previous_fiscal_year.php
+++ application/controllers/cron/service_graphical_dashboard_cron_previous_fiscal_year.php
@@ -468,7 +468,7 @@ class Service_graphical_dashboard_cron_previous_fiscal_year extends crm_controll
 					if(isset($mon_revenue) && $mon_revenue != 0) {
 						$inse_array[$con_month] = round((($mon_revenue - $mon_contrib)/$mon_revenue)*100);
 					}
-					echo '<pre> Mon - '. $fis_mon . ' Revenue - ' .$mon_revenue . ' Contribu - ' .$mon_contrib; echo '<br />';
+					echo $parr.'-  Mon - '. $fis_mon . ' Revenue - ' .$mon_revenue . ' Contribu - ' .$mon_contrib; echo '<br />';
 					/* $this->db->where(array('practice_name' => $parr));
 					$this->db->update($this->cfg['dbpref'] . 'services_graphical_dashboard_last_fiscal_year', $inse_array); */
 					// echo $this->db->last_query() . "<br />";
