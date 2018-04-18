265fa447f0b2be011f6d73a58a33612ae8413734
myvizhi.v <myvizhi.v@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2018-04-18 12:25:10 +0000

    test - cost report projects filter

--- application/controllers/projects/dashboard.php
+++ application/controllers/projects/dashboard.php
@@ -1346,7 +1346,7 @@ class Dashboard extends crm_controller
 					$qry = $timesheet_db->query("SELECT DISTINCT(project_code), title FROM `enoah_project` where project_code in ('$each_code')");
 					$pro_title = array();
 					if($qry->num_rows()>0){
-						$res_code = $qry->result();//echo'<pre>hello result final===>';print_r($res_code);exit;
+						$res_code = $qry->result();echo'<pre>hello result final===>';print_r($res_code['0']->title);exit;
 						$pro_title[] = $res_code['0']->title;
 					}
 				}echo'<pre>hello result final===>';print_r($pro_title);exit;
