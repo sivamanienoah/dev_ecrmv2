6ffb28d8c3fe47b7b55aecffd62cad676a4c9008
myvizhi.v <myvizhi.v@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2018-04-18 09:15:52 +0000

    test - cost report projects filter

--- application/views/themes/eNoah/projects/cost_report_view.php
+++ application/views/themes/eNoah/projects/cost_report_view.php
@@ -399,23 +399,19 @@ $(document).ready(function(){
 		});
 	});
 	
-	$('body').on('change','#member_ids',function(){
+	/* $('body').on('change','#member_ids',function(){
 		var dids       = $('#department_ids').val();
 		var start_date = $('#month_year_from_date').val();
 		var end_date   = $('#month_year_to_date').val();
-		var resourceids = $(this).val();
+		var sids = $(this).val();
 		$("#filter_area_status").val('1');
 		$('#member_ids').html('');
-		alert(dids);
-		alert(start_date);
-		alert(end_date);
-		alert(resourceids);
-		var params = { 'dept_ids':dids,'resource_ids':resourceids,'start_date':start_date,'end_date':end_date };
+		var params = { 'dept_ids':dids,'skill_ids':sids,'start_date':start_date,'end_date':end_date };
 		params[csrf_token_name] = csrf_hash_token;
 		
 		$.ajax({
 			type: 'POST',
-			url: site_base_url+'projects/dashboard/get_projects_by_members',
+			url: site_base_url+'projects/dashboard/get_skill_members',
 			data: params,
 			success: function(members) {
 				if(members){
@@ -431,7 +427,7 @@ $(document).ready(function(){
 				}
 			}
 		});
-	});
+	}); */
 	
 	$("input[name=exclude_leave]").prop("checked",true);
 	$("input[name=exclude_holiday]").prop("checked",true);
