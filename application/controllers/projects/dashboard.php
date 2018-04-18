5484b0f8e8f9d572cae862d07f3fa441456501e1
myvizhi.v <myvizhi.v@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2018-04-18 12:05:58 +0000

    test - cost report projects filter

--- application/controllers/projects/dashboard.php
+++ application/controllers/projects/dashboard.php
@@ -1335,14 +1335,18 @@ class Dashboard extends crm_controller
 					$each_project_codes[] = $each_res->project_code;
 				}
 				echo'<pre>hello===>';print_r($each_project_codes);
-				$each_pjct_codes = implode(',',$each_project_codes);
+				// $each_pjct_codes = implode(',',$each_project_codes);
 				
 				
-				$dids = implode(',',$ids);
+				// $dids = implode(',',$ids);
 				$timesheet_db = $this->load->database("timesheet",true);
-				
-				$qry = $timesheet_db->query("SELECT DISTINCT(project_code), title FROM ".$timesheet_db->dbprefix('project')." where project_code in ($each_pjct_codes)");
-				
+				foreach($each_project_codes as $key => $each_code){
+					$qry = $timesheet_db->query("SELECT DISTINCT(project_code), title FROM ".$timesheet_db->dbprefix('project')." where project_code in ($each_code)");
+					$res_code = array();
+					if($qry->num_rows()>0){
+						$res_code[] = $qry->result();					
+					}
+				}echo'<pre>hello result final===>';print_r($res_code);exit;
 				// $qry = $timesheet_db->query("SELECT v.username,concat(v.first_name,' ',v.last_name) as emp_name FROM `v_emp_details` v join enoah_times t on v.username=t.uid where v.department_id in ($dids) and t.start_time between '$start_date' and '$end_date' group by v.username order by v.username asc");
 				if($qry->num_rows()>0){
 					$res = $qry->result();echo'<pre>hello result===>';print_r($res);exit;
