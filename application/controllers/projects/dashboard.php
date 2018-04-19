a13de295f9ad48f86b6c57e2ef3eccb6ebce201a
myvizhi.v <myvizhi.v@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2018-04-19 07:30:47 +0000

    test

--- application/controllers/projects/dashboard.php
+++ application/controllers/projects/dashboard.php
@@ -4588,7 +4588,7 @@ class Dashboard extends crm_controller
 	private function get_default_projects($start_date, $end_date)
 	{
 		$this->db->distinct('l.lead_title');
-		$this->db->select('t.dept_id, t.dept_name, t.skill_id, t.skill_name, t.resoursetype, t.username, t.duration_hours, t.resource_duration_cost, t.cost_per_hour, t.project_code, t.empname, t.direct_cost_per_hour, t.resource_duration_direct_cost,t.entry_month as month_name, t.entry_year as yr, t.entity_id, t.entity_name, p.id as practice_id, p.practices as practice_name, l.lead_title');
+		$this->db->select('t.dept_id, t.dept_name, t.project_code, l.lead_title');
 		// t.practice_id, t.practice_name
 		$this->db->from($this->cfg['dbpref']. 'timesheet_month_data as t');
 		$this->db->join($this->cfg['dbpref']. 'leads as l', 'l.pjt_id = t.project_code', 'LEFT');
@@ -4605,7 +4605,7 @@ class Dashboard extends crm_controller
 		$this->db->where('l.practice is not null');
 		$this->db->where('l.lead_title is not null');
 		$query 	= $this->db->get();	
-		// echo $this->db->last_query(); die;
+		echo $this->db->last_query(); die;
 		return $query->result();
 	}
 	
