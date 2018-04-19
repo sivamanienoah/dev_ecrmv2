da815e7b40b6e0d434a2711b468325a25ae06acf
myvizhi.v <myvizhi.v@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2018-04-19 11:13:43 +0000

    test

--- application/controllers/projects/dashboard.php
+++ application/controllers/projects/dashboard.php
@@ -3967,14 +3967,14 @@ class Dashboard extends crm_controller
 		}
 		$this->db->where('l.practice is not null');
 		$query 						= $this->db->get();		
-		// echo $this->db->last_query();
+		echo $this->db->last_query();
 		$resdata  = $query->result();
 		$data['heading'] 	   		= $heading;
 		$data['dept_type']     		= $dept_type;
 		$data['resource_type'] 		= $resource_type;
 		$conversion_rates 	= $this->get_currency_rates();
 		
-		// echo '<pre>'; print_r($resdata); die;
+		echo '<pre>'; print_r($resdata); die;
 		
 		// get all projects from timesheet
 		$timesheet_db = $this->load->database("timesheet", true);
