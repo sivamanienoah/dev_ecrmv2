acb8bcf2524c748e76851e09bd4027a1e725cd19
myvizhi.v <myvizhi.v@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2018-04-19 07:23:25 +0000

    project list query for eQAD and eADS dept

--- application/controllers/projects/dashboard.php
+++ application/controllers/projects/dashboard.php
@@ -3758,7 +3758,7 @@ class Dashboard extends crm_controller
 		}
 		$this->db->where('l.practice is not null');
 		$query 						= $this->db->get();		
-		echo $this->db->last_query(); exit;
+		// echo $this->db->last_query(); exit;
 		$data['resdata'] 	   		= $query->result();//echo'<pre>';print_r($data['resdata']);exit;
 		$data['heading'] 	   		= $heading;
 		$data['dept_type']     		= $dept_type;
