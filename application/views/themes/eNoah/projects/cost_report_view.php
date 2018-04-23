358ea3a77527f215d14b0ed185a8a7bf7559caf7
myvizhi.v <myvizhi.v@abe59b9f-38d8-47bd-ac26-6ad9e1cfceae>
2018-04-23 10:50:07 +0000

    test

--- application/views/themes/eNoah/projects/cost_report_view.php
+++ application/views/themes/eNoah/projects/cost_report_view.php
@@ -126,7 +126,7 @@ table.bu-tbl-inr th{ text-align:center; }
 											<?php } }?>
 										</select>
 									</td>
-									<td>
+									<td class="proj-dash-select">
 										<select id="member_ids" name="member_ids[]" multiple="multiple">
 											<?php if(count($member_ids_selected)>0 && !empty($member_ids_selected)){?>
 											<?php foreach($member_ids_selected as $members){?>
